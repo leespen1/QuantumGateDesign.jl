9db5b5f9dcff9588a43b7041305a5fb056022d3e
Spencer Lee <leespen1@msu.edu>
2023-01-17 15:09:36 -0500

    Fixed differing initial conditions between analytic solution and numerical solution

--- 1d_example.jl
+++ 1d_example.jl
@@ -1,7 +1,10 @@
 # Testing on 1D Problem
 # dpsidt = i*sint*a*psi
+using Plots
+using LinearAlgebra
+using LaTeXStrings
 
-function compute_gradient(N)
+function compute_gradient(N, fT)
     S(t, a) = 0.0
     K(t, a) = sin(t)*a
     M(t, a) = [S(t, a) -K(t,a)
@@ -11,52 +14,71 @@ function compute_gradient(N)
     E = 1 # 1 essential energy level
 
     a = 1.0 # Current value of control parameter
-    Q0 = [1.0; 0.0] # psi_0= 1+0im, initial condition
-    fT = 4 # Final time
+    #Q0 = [1.0; 0.0] # psi_0= 1+0im, initial condition
+    psi0 = exp(-1im*cos(0)*a)
+    Q0 = [real(psi0), imag(psi0)]
 
-    dt = fT/(N-1) # Timestep size
-    Qs = zeros(2,N) # For storing state vector evolution (in larger problems wouldn't store them all)
+    dt = fT/N # Timestep size
+    Qs = zeros(2,N+1) # For storing state vector evolution (in larger problems wouldn't store them all)
     Qs[:,1] = Q0
 
     I = [1.0 0.0
          0.0 1.0]
 
     # Forward Evolve
-    for n in 1:N-1
-        tn = (n-1)*dt
-        tnp1 = n*dt
-        Qs[:,n+1] = (I - 0.5*dt*M(tnp1, a)) \ (I + 0.5*dt*M(tn, a))*Qs[:,n]
+    for n in 0:N-1
+        tn = n*dt
+        tnp1 = (n+1)*dt
+        Qs[:,1+n+1] = (I - 0.5*dt*M(tnp1, a)) \ ((I + 0.5*dt*M(tn, a))*Qs[:,1+n])
         #println("$tn, $tnp1")
     end
-    #println("Qs: $Qs")
+    #println("Qs")
+    #display(Qs)
 
     # Set target gate (to QN, in order to test the method; gradient should be zero)
-    QN = Qs[:,N]
+    QN = Qs[:,1+N]
     R = QN[:] # copy
     T = [R[2], -R[1]]
 
 
-    lambdas = zeros(2,N) # For storing adjoint state vector solutions
+    lambdas = zeros(2,N+1) # For storing adjoint state vector solutions
     # Terminal condition
-    lambdas[:,N] = (I - 0.5*dt*M(fT, a)') \ ((2/E^2)*((QN'*R)*R + (QN'*T)*T)) # guard-level term is 0 in 1D
+    lambdas[:,1+N] = (I - 0.5*dt*M(fT, a)') \ ((2/E^2)*((QN'*R)*R + (QN'*T)*T)) # guard-level term is 0 in 1D
 
     # Backward/Adjoint Evolve
     for n in N-1:-1:1
-        tn = (n-1)*dt
-        tnp1 = n*dt
-        lambdas[:,n] = (I - 0.5*dt*M(tn, a)') \ (I + 0.5*dt*M(tn, a)')*lambdas[:,n+1] # No guard-level forcing term because we are in 1D
-        #println("$tn, $tnp1")
+        tn = n*dt
+        lambdas[:,1+n] = (I - 0.5*dt*M(tn, a)') \ (I + 0.5*dt*M(tn, a)')*lambdas[:,1+n+1] # No guard-level forcing term because we are in 1D
     end
-    #println("Λs: $lambdas")
+    #println("Λs:")
+    #display(lambdas)
 
     # Calculate gradient
     gradient = 0.0
-    for n in 1:N-1
-        tn = (n-1)*dt
-        tnp1 = n*dt
-        gradient += (dMda(tn)*Qs[:,n] + dMda(tnp1)*Qs[:,n+1])'*lambdas[:,n+1]
+    for n in 0:N-1
+        tn = n*dt
+        tnp1 = (n+1)*dt
+        gradient += (dMda(tn)*Qs[:,1+n] + dMda(tnp1)*Qs[:,1+n+1])'*lambdas[:,1+n+1]
     end
     gradient *= -0.5*dt
-    println("Gradient: $gradient")
+    #println("Gradient: $gradient")
     return gradient, QN
+end
+
+function graph(Ns, fT)
+    gradients = zeros(length(Ns))
+    QNs = zeros(2,length(Ns))
+    for i in 1:length(Ns)
+        N = Ns[i]
+        gradients[i], QNs[:,i] = compute_gradient(N, fT)
+    end
+
+    psifT = exp(-im*cos(fT))
+    QN_true = [real(psifT), imag(psifT)]
+
+    accuracies = [norm(QN - QN_true) for QN in eachcol(QNs)]*(1/norm(QN_true))
+    pl_acc = plot(Ns, accuracies, xlabel="N", ylabel="Relative Error", scale=:log10,
+                  title=L"\dot{\psi} = i \sin(t) \alpha \psi(t),\quad \alpha = 1")
+    pl_grad = plot(Ns, gradients, xlabel="N", ylabel=L"||\nabla_\alpha \mathcal{J}_h||")
+    return [pl_acc, pl_grad]
 end
