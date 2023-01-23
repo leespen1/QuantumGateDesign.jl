19fb91d2f23d1513b5d35e4b69adfe80fc64c753
Spencer Lee <leespen1@msu.edu>
2023-01-23 11:06:46 -0500

    Added an even simpler example

--- 1d_example.jl
+++ 1d_example.jl
@@ -4,7 +4,7 @@ using Plots
 using LinearAlgebra
 using LaTeXStrings
 
-function compute_gradient(N, fT)
+function compute_gradient(N, fT, psi0=1.0)
     S(t, a) = 0.0
     K(t, a) = cos(t)*a
     M(t, a) = [S(t, a) -K(t,a)
@@ -16,7 +16,6 @@ function compute_gradient(N, fT)
     a = 1.0 # Current value of control parameter
     #Q0 = [1.0; 0.0] # psi_0= 1+0im, initial condition
     #psi0 = exp(1im*sin(0)*a)
-    psi0 = 1
     Q0 = [real(psi0), imag(psi0)]
 
     dt = fT/N # Timestep size
@@ -71,7 +70,7 @@ function graph(Ns, fT, psi0=1.0)
     QNs = zeros(2,length(Ns))
     for i in 1:length(Ns)
         N = Ns[i]
-        gradients[i], QNs[:,i] = compute_gradient(N, fT)
+        gradients[i], QNs[:,i] = compute_gradient(N, fT, psi0)
     end
 
     psifT = exp(im*sin(fT))*psi0
@@ -82,4 +81,77 @@ function graph(Ns, fT, psi0=1.0)
                   title=L"\dot{\psi} = i \cos(t) \alpha \psi(t),\quad \alpha = 1")
     pl_grad = plot(Ns, gradients, xlabel="N", ylabel=L"||\nabla_\alpha \mathcal{J}_h||")
     return [pl_acc, pl_grad]
+end
+
+function compute_gradient_simpler(N, fT, psi0=1.0)
+    S(t, a) = 0.0
+    K(t, a) = a
+    M(t, a) = [S(t, a) -K(t,a)
+               K(t, a) S(t,a)]
+    dMda(t) = [0.0 -1.0
+               1.0 0.0]
+    E = 1 # 1 essential energy level
+
+    a = 0.0 # Current value of control parameter
+    #Q0 = [1.0; 0.0] # psi_0= 1+0im, initial condition
+    #psi0 = exp(1im*sin(0)*a)
+    Q0 = [real(psi0), imag(psi0)]
+
+    dt = fT/N # Timestep size
+    Qs = zeros(2,N+1) # For storing state vector evolution (in larger problems wouldn't store them all)
+    Qs[:,1] = Q0
+
+    I = [1.0 0.0
+         0.0 1.0]
+
+    # Forward Evolve
+    for n in 0:N-1
+        tn = n*dt
+        tnp1 = (n+1)*dt
+        Qs[:,1+n+1] = (I - 0.5*dt*M(tnp1, a)) \ ((I + 0.5*dt*M(tn, a))*Qs[:,1+n])
+        #println("$tn, $tnp1")
+    end
+    #println("Qs")
+    #display(Qs)
+
+    # Set target gate (to QN, in order to test the method; gradient should be zero)
+    QN = Qs[:,1+N]
+    R = QN[:] # copy
+    T = [R[2], -R[1]]
+    println("Qs:")
+    display(Qs)
+    println("Qₙ:")
+    display(Qs[:,1+N])
+    println("R:")
+    display(R)
+    println("T:")
+    display(T)
+
+
+    lambdas = zeros(2,N+1) # For storing adjoint state vector solutions
+    # Terminal condition
+    lambdas[:,1+N] = (I - 0.5*dt*M(fT, a)') \ ((2/E^2)*((QN'*R)*R + (QN'*T)*T)) # guard-level term is 0 in 1D
+    println("Λₙ:")
+    display(lambdas[:,1+N])
+
+    # Backward/Adjoint Evolve
+    for n in N-1:-1:1
+        tn = n*dt
+        lambdas[:,1+n] = (I - 0.5*dt*M(tn, a)') \ (I + 0.5*dt*M(tn, a)')*lambdas[:,1+n+1] # No guard-level forcing term because we are in 1D
+    end
+    #println("Λs:")
+    #display(lambdas)
+
+    # Calculate gradient
+    gradient = 0.0
+    for n in 0:N-1
+        tn = n*dt
+        tnp1 = (n+1)*dt
+        gradient += (dMda(tn)*Qs[:,1+n] + dMda(tnp1)*Qs[:,1+n+1])'*lambdas[:,1+n+1]
+        display(lambdas[:,1+n+1])
+        println("gradient = $gradient")
+    end
+    gradient *= -0.5*dt
+    #println("Gradient: $gradient")
+    return gradient, QN
 end
