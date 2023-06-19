bdc7b1714f08021c33509c3783b4f05a101323da
Spencer Lee <leespen1@msu.edu>
2023-06-19 12:06:24 -0600

    Adding routine tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,11 @@
 using Test
+
+prob = gargamel_prob()
+pcof = rand(4)
+
+history = eval_forward(prob, pcof)
+
+target = rand(4,2)
+eval_grad_finite_difference(prob, target, alpha, cost_type=:Infidelity)
+eval_grad_finite_difference(prob, target, alpha, cost_type=:Tracking)
+eval_grad_finite_difference(prob, target, alpha, cost_type=:Norm)
