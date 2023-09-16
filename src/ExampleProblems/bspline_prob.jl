783cc201252cd3d7144f906e17f8000ce4484e15
Spencer Lee <leespen1@msu.edu>
2023-09-15 17:26:37 -0700

    Added constructor for vector initial condition for bspline problems, in addition to matrix one

--- src/ExampleProblems/bspline_prob.jl
+++ src/ExampleProblems/bspline_prob.jl
@@ -78,3 +78,34 @@ function bspline_prob(ω::Float64=0.0; tf::Float64=1.0, nsteps::Int64=10)
                            p,q,dpdt,dqdt,dpda,dqda,d2p_dta,d2q_dta,
                            u0,v0,tf,nsteps,N_essential,N_guard)
 end
+
+function bspline_prob_vec(ω::Float64=0.0; tf::Float64=1.0, nsteps::Int64=10)
+    Ks::Matrix{Float64} = [0 0; 0 1]
+    Ss::Matrix{Float64} = [0 0; 0 0]
+    a_plus_adag::Matrix{Float64} = [0.0 1.0; 1.0 0.0]
+    a_minus_adag::Matrix{Float64} = [0.0 1.0; -1.0 0.0]
+
+    T = 1.0
+    D1 = 4 # Number of B-spline coefficients per control function
+    omega = [[ω]] # 1 frequency for 1 pair of coupled controls (p and q)
+    pcof = ones(2*D1)
+    # Use simplest constructor
+    bcpar = bcparams(T, D1,omega, pcof) 
+
+    p(t,α) = bspline_p(t,α,bcpar)
+    q(t,α) = bspline_q(t,α,bcpar)
+    dpdt(t,α) = bspline_dpdt(t,α,bcpar)
+    dqdt(t,α) = bspline_dqdt(t,α,bcpar)
+    dpda(t,α) = bspline_dpda(t,α,bcpar)
+    dqda(t,α) = bspline_dqda(t,α,bcpar)
+    d2p_dta(t,α) = bspline_d2pdta(t,α,bcpar)
+    d2q_dta(t,α) = bspline_d2qdta(t,α,bcpar)
+
+    u0::Vector{Float64} = [1,0]
+    v0::Vector{Float64} = [0,0]
+    N_essential = 2
+    N_guard = 0
+    return SchrodingerProb(Ks,Ss, a_plus_adag, a_minus_adag,
+                           p,q,dpdt,dqdt,dpda,dqda,d2p_dta,d2q_dta,
+                           u0,v0,tf,nsteps,N_essential,N_guard)
+end
