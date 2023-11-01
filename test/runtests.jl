08f7a88f263e06f3c983d614c7119da844d317f4
Spencer Lee <leespen1@msu.edu>
2023-10-31 17:18:16 -0700

    Reworked gradient test for new control structure

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using HermiteOptimalControl
-using Test
+using Test: @testset
 
 #=
 # Test suite
@@ -20,6 +20,9 @@ N_coeff_per_control = 4
 
 tf=1.0
 nsteps = 5 # Use few steps for tests, care about algorithm correctness, not physical accuracy
+# EDIT: Forced and Discrete Adjoint gradients seem to deviate as number of
+# steps increase. Is this numerical saturation / double precision breakdown, or
+# something worse?
 
 prob, control = HermiteOptimalControl.qubit_with_bspline(
     detuning_frequency,
@@ -37,44 +40,15 @@ pcof = rand(8)
 orders = [2, 4]
 cost_types = [:Infidelity, :Tracking, :Norm]
 
-@testset "Checking Gradient Agreement Between Methods" begin
-
-# Check that gradients calculated using discrete adjoint and finite difference
-# methods agree to reasonable precision
-@testset "Discrete Adjoint Vs Finite Difference Vs Forced Method" begin
-for cost_type in cost_types
-    @testset "Cost function: $cost_type" begin
-    for order in orders
-        @testset "Order: $order" begin
-        grad_disc_adj = discrete_adjoint(prob, control, pcof, target, order=order, cost_type=cost_type)
-
-        grad_forced = eval_grad_forced(prob, control, pcof, target, order=order, cost_type=cost_type)
-        grad_fin_diff = eval_grad_finite_difference(prob, control, pcof, target, order=order, cost_type=cost_type)
-
-        forced_atol = 1e-14
-        fin_diff_atol = 1e-9 # Might want to relax this to 1e-9.
-
-        @testset "Forced Method" begin
-            for k in 1:length(grad_disc_adj)
-                @test isapprox(grad_disc_adj[k], grad_forced[k], atol=forced_atol)
-            end
-        end
-
-        @testset "Finite Difference" begin
-            for k in 1:length(grad_disc_adj)
-                @test isapprox(grad_disc_adj[k], grad_fin_diff[k], atol=fin_diff_atol)
-            end
-        end
-
-        end #@testset "Order: $order"
-    end
-    end #@testset "Cost function: $cost_type"
-end
-end # @testset "Discrete Adjoint Vs Finite Difference Vs Forced Method"
-
+@testset "Gradient Agreement Between Methods" begin
+    HermiteOptimalControl.test_gradient_agreement(
+        prob, control, pcof, target, orders=orders, cost_types=cost_types
+    )
 end # @testset "Checking Gradient Agreement Between Methods"
 
 @testset "Covergence" begin
 # Should check that convergence of forward evolution is of the correct order.
 end
+
+
 
