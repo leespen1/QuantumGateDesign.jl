f5ac64925196549d2c338b96b4bd174ca4937575
Spencer Lee <leespen1@msu.edu>
2023-11-01 06:14:11 -0700

    Removed old functinos

--- src/ExampleProblems/bspline_prob.jl
+++ src/ExampleProblems/bspline_prob.jl
@@ -30,7 +30,7 @@ end
 """
 Single qubit in the rotating frame, with rotating wave approximation.
 """
-function qubit_with_bspline(detuning_frequency,
+function single_qubit_prob_with_bspline_control(detuning_frequency,
         self_kerr_coefficient, N_ess_levels, N_guard_levels;
         N_coeff_per_control = 4, tf=1.0, nsteps=10, 
     )
@@ -49,66 +49,3 @@ function qubit_with_bspline(detuning_frequency,
 
     return prob, control
 end
-#=
-# Expects pcof of length 8
-"""
-    prob = bspline_prob(ω::Float64; tf, nsteps)
-
-Construct a problem for a 2-level qubit in the rotating frame with a B-spline
-control using a real-valued control vector of length 8 (the first half is the
-real part and the second half is the imaginary part in the
-complex-formulation).
-"""
-function bspline_prob(ω::Float64=0.0; n_levels::Int=1, tf::Float64=1.0, nsteps::Int=10)
-    n_subsystems = 1
-    system_hamiltonian = get_system_hamiltonian(n_subsystems, n_levels)
-
-    Ss::Matrix{Float64} = [0 0; 0 0]
-    a_plus_adag::Matrix{Float64} = [0.0 1.0; 1.0 0.0]
-    a_minus_adag::Matrix{Float64} = [0.0 1.0; -1.0 0.0]
-
-    T = 1.0
-    D1 = 4 # Number of B-spline coefficients per control function
-    omega = [[ω]] # 1 frequency for 1 pair of coupled controls (p and q)
-    pcof = ones(2*D1)
-    # Use simplest constructor
-    bcpar = bcparams(T, D1,omega, pcof) 
-
-    N_essential = 2
-    N_guard = 0
-    return SchrodingerProb(Ks,Ss, a_plus_adag, a_minus_adag,
-                           p,q,dpdt,dqdt,dpda,dqda,d2p_dta,d2q_dta,
-                           u0,v0,tf,nsteps,N_essential,N_guard)
-end
-
-function bspline_prob_vec(ω::Float64=0.0; tf::Float64=1.0, nsteps::Int64=10)
-    Ks::Matrix{Float64} = [0 0; 0 1]
-    Ss::Matrix{Float64} = [0 0; 0 0]
-    a_plus_adag::Matrix{Float64} = [0.0 1.0; 1.0 0.0]
-    a_minus_adag::Matrix{Float64} = [0.0 1.0; -1.0 0.0]
-
-    T = 1.0
-    D1 = 4 # Number of B-spline coefficients per control function
-    omega = [[ω]] # 1 frequency for 1 pair of coupled controls (p and q)
-    pcof = ones(2*D1)
-    # Use simplest constructor
-    bcpar = bcparams(T, D1,omega, pcof) 
-
-    p(t,α) = bspline_p(t,α,bcpar)
-    q(t,α) = bspline_q(t,α,bcpar)
-    dpdt(t,α) = bspline_dpdt(t,α,bcpar)
-    dqdt(t,α) = bspline_dqdt(t,α,bcpar)
-    dpda(t,α) = bspline_dpda(t,α,bcpar)
-    dqda(t,α) = bspline_dqda(t,α,bcpar)
-    d2p_dta(t,α) = bspline_d2pdta(t,α,bcpar)
-    d2q_dta(t,α) = bspline_d2qdta(t,α,bcpar)
-
-    u0::Vector{Float64} = [1,0]
-    v0::Vector{Float64} = [0,0]
-    N_essential = 2
-    N_guard = 0
-    return SchrodingerProb(Ks,Ss, a_plus_adag, a_minus_adag,
-                           p,q,dpdt,dqdt,dpda,dqda,d2p_dta,d2q_dta,
-                           u0,v0,tf,nsteps,N_essential,N_guard)
-end
-=#
