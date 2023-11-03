2e2e175448babe4bffbab200365e4ea25005b9b7
Spencer Lee <leespen1@msu.edu>
2023-11-02 18:30:34 -0700

    Added rtol to tests, also added options for printing results

--- test/test_gradient.jl
+++ test/test_gradient.jl
@@ -1,16 +1,19 @@
 using Test: @test, @testset
 
 function test_gradient_agreement(prob, control, pcof, target; 
-        orders=[2, 4], cost_types=[:Infidelity, :Tracking, :Norm]
+        orders=[2, 4], cost_types=[:Infidelity, :Tracking, :Norm],
+        print_results=false
     )
+
+    grad_diff_norms = Array{Float64, 3}(undef, 2, length(orders), length(cost_types))
 
     # Check that gradients calculated using discrete adjoint and finite difference
     # methods agree to reasonable precision
     @testset "Discrete Adjoint Vs Finite Difference Vs Forced Method" begin
-    for cost_type in cost_types
-        @testset "Cost function: $cost_type" begin
-        for order in orders
-            @testset "Order: $order" begin
+        for (order_ind, order) in enumerate(orders)
+        @testset "Order: $order" begin
+            for (cost_ind, cost_type) in enumerate(cost_types)
+            @testset "Cost function: $cost_type" begin
                 grad_disc_adj = discrete_adjoint(
                     prob, control, pcof, target, order=order,
                     cost_type=cost_type
@@ -31,14 +34,31 @@ function test_gradient_agreement(prob, control, pcof, target;
 
                 @testset "Forced Method" begin
                     for k in 1:length(grad_disc_adj)
-                        @test isapprox(grad_disc_adj[k], grad_forced[k], atol=forced_atol)
+                        @test isapprox(grad_disc_adj[k], grad_forced[k], atol=forced_atol, rtol=forced_atol)
                     end
                 end
 
                 @testset "Finite Difference" begin
                     for k in 1:length(grad_disc_adj)
-                        @test isapprox(grad_disc_adj[k], grad_fin_diff[k], atol=fin_diff_atol)
+                        @test isapprox(grad_disc_adj[k], grad_fin_diff[k], atol=fin_diff_atol, rtol=forced_atol)
                     end
+                end
+
+                disc_adj_minus_forced = grad_disc_adj - grad_forced
+                disc_adj_minus_fin_diff = grad_disc_adj - grad_fin_diff
+
+                grad_diff_norms[1, order_ind, cost_ind] = norm(disc_adj_minus_forced)
+                grad_diff_norms[2, order_ind, cost_ind] = norm(disc_adj_minus_fin_diff)
+
+                if print_results
+                    println("Order=$order, cost_type=$cost_type")
+                    println("--------------------------------------------------")
+                    println("Discrete Adjoint Gradient:\n", grad_disc_adj)
+                    println("\nForced Method Gradient:\n", grad_forced)
+                    println("\nFinite Difference Gradient:\n", grad_fin_diff)
+                    println("\n\nDiscrete Adjoint - Forced Method:\n", disc_adj_minus_forced)
+                    println("Discrete Adjoint - Finite Difference:\n", disc_adj_minus_fin_diff)
+                    println("\n\n")
                 end
 
             end #@testset "Order: $order"
@@ -46,4 +66,6 @@ function test_gradient_agreement(prob, control, pcof, target;
         end #@testset "Cost function: $cost_type"
     end
     end # @testset "Discrete Adjoint Vs Finite Difference Vs Forced Method"
+
+    return grad_diff_norms
 end
