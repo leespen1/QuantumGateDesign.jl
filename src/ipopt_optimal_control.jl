a3ec1433e27ead4a6048d279c68685991c9e4853
Spencer Lee <leespen1@msu.edu>
2023-11-04 16:40:45 -0700

    Added multiple orders, and options for ipopt

--- src/ipopt_optimal_control.jl
+++ src/ipopt_optimal_control.jl
@@ -46,25 +46,26 @@ Unused, but need a function to provide to ipopt.
 """
 function optimize_gate(
         schro_prob::SchrodingerProb{M, VM}, control::Control,
-        pcof_init::AbstractVector{Float64}, target::VM
+        pcof_init::AbstractVector{Float64}, target::VM;
+        order=4
     ) where {V<:AbstractVector{Float64}, VM<:AbstractVecOrMat{Float64}, M<:AbstractMatrix{Float64}}
 
     # Right now I am unnecessarily doing a full forward evolution to compute the
     # infidelity, when this should be done already in the gradient computation.
     function eval_f(pcof::Vector{Float64})
         println(pcof)
-        history = eval_forward(schro_prob, control, pcof)
+        history = eval_forward(schro_prob, control, pcof, order=order)
         QN = history[:,end,:]
         return infidelity(QN, target, schro_prob.N_ess_levels)
     end
 
     function eval_grad_f!(pcof::Vector{Float64}, grad_f::Vector{Float64})
-        grad_f .= discrete_adjoint(schro_prob, control, pcof, target)
+        grad_f .= discrete_adjoint(schro_prob, control, pcof, target, order=order)
     end
 
     N_coeff = length(pcof_init)
     x_L = zeros(N_coeff)
-    x_U = ones(N_coeff) .* 100
+    x_U = ones(N_coeff) .* 2
 
     N_constraints = 0
     g_L = Vector{Float64}()
@@ -89,14 +90,27 @@ function optimize_gate(
         dummy_eval_hessian!,
     )
 
+    maxIter = 50
+    lbfgsMax = 200
+    coldStart=true
+    acceptTol=1e-5
+    ipTol = 1e-5
+    acceptIter = 1
+
+    #Ipopt.AddIpoptIntOption(ipopt_prob, "acceptable_iter", acceptIter)
+    Ipopt.AddIpoptStrOption(ipopt_prob, "hessian_approximation", "limited-memory"); # Use L-BFGS, approximate hessian
+    Ipopt.AddIpoptIntOption(ipopt_prob, "limited_memory_max_history", lbfgsMax);
+    Ipopt.AddIpoptIntOption(ipopt_prob, "max_iter", maxIter);
+    Ipopt.AddIpoptNumOption(ipopt_prob, "tol", ipTol);
+    Ipopt.AddIpoptNumOption(ipopt_prob, "acceptable_tol", acceptTol);
+    Ipopt.AddIpoptIntOption(ipopt_prob, "acceptable_iter", acceptIter);
+    Ipopt.AddIpoptStrOption(ipopt_prob, "jacobian_approximation", "exact");
+    Ipopt.AddIpoptStrOption(ipopt_prob, "derivative_test", "first-order") # What does this do?
+
     #=
-    addOption( prob, "hessian_approximation", "limited-memory");
-    addOption( prob, "limited_memory_max_history", lbfgsMax); # Max number of past iterations for hessian approximation
-    addOption( prob, "max_iter", maxIter);
-    addOption( prob, "tol", ipTol);
-    addOption( prob, "acceptable_tol", acceptTol);
-    addOption( prob, "acceptable_iter", acceptIter);
-    addOption( prob, "jacobian_approximation", "exact");
+    addOption(ipopt_prob, "hessian_approximation", "limited-memory");
+    addOption(ipopt_prob, "limited_memory_max_history", lbfgsMax); # Max number of past iterations for hessian approximation
+    addOption(ipopt_prob, "jacobian_approximation", "exact");
     =#
 
     ipopt_prob.x .= pcof_init
