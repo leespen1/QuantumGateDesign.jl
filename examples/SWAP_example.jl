7274c4e5a6d9226171ddae71c9b4cd237ec1ac05
Spencer Lee <leespen1@msu.edu>
2023-11-05 01:54:39 -0800

    Changed default parameters

--- examples/SWAP_example.jl
+++ examples/SWAP_example.jl
@@ -24,14 +24,16 @@
 
 using HermiteOptimalControl
 
-function main(N_ess_levels=2, N_guard_levels=0)
+function main(d=1, N_guard_levels=2)
+    N_ess_levels = d+1
     # Set up problem
-    tf = 140.0 # If everything else is in GHz, then I think 140 should be 140 ns
+    tf = 50.0 # If everything else is in GHz, then I think tf should be in ns
     nsteps=10
     #nsteps = 14787
 
     detuning_frequency = 0.0
     self_kerr_coefficient =  2*pi*0.22 
+
 
     prob = rotating_frame_qubit(
         N_ess_levels,
@@ -44,16 +46,17 @@ function main(N_ess_levels=2, N_guard_levels=0)
 
     # Set up control
     D1 = 10
-    carrier_wave_freqs = [(k-1)*(-self_kerr_coefficient) for k in 1:N_ess_levels]
+    carrier_wave_freqs = [(k-1)*(-self_kerr_coefficient) for k in 1:d]
 
     control = bspline_control(tf, D1, carrier_wave_freqs)
 
-    pcof = rand(2*D1*N_ess_levels)
+    pcof = rand(control.N_coeff)
 
     SWAP_target_complex = zeros(N_ess_levels,N_ess_levels)
-    for i in 1:N_ess
-        SWAP_target_complex[1+N_ess_levels-i,i] = 1
+    for i in 2:N_ess_levels-1
+        SWAP_target_complex[i,i] = 1
     end
+    SWAP_target_complex[1,N_ess_levels] = SWAP_target_complex[N_ess_levels,1] = 1
 
     SWAP_target_real = target_helper(SWAP_target_complex, N_guard_levels)
 
