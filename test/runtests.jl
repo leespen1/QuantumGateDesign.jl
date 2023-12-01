7ce01a9ae972b7b86138679ba181055e7c8973b2
Spencer Lee <leespen1@msu.edu>
2023-11-30 21:56:30 -0800

    Rearranged tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using HermiteOptimalControl
-using Test: @testset
+using Test: @testset, @test
+
 
 #=
 # Test suite
@@ -10,40 +11,73 @@ using Test: @testset
 # the exact same results up to machine precision.
 #
 =#
+@testset "Constructing Typical Problem Without Throwing Exception" begin
+end
+
+
+@testset "Constructing Control Without Throwing Exception" begin
+end
+
+N_ess = 2
+N_guard = 2
+N_tot_levels = N_ess + N_guard
+
 detuning_frequency = 1.0
 self_kerr_coefficient = 0.5
 
-N_ess = 2
-N_guard = 0
-
-N_coeff_per_control = 4
-
 tf=1.0
-nsteps = 5 # Use few steps for tests, care about algorithm correctness, not physical accuracy
-# EDIT: Forced and Discrete Adjoint gradients seem to deviate as number of
-# steps increase. Is this numerical saturation / double precision breakdown, or
-# something worse?
+nsteps = 5
 
-prob, control = HermiteOptimalControl.qubit_with_bspline(
-    detuning_frequency,
-    self_kerr_coefficient,
+prob = rotating_frame_qubit(
     N_ess,
     N_guard,
-    tf=tf,
-    nsteps=nsteps,
-    N_coeff_per_control=N_coeff_per_control
+    tf = tf,
+    nsteps = nsteps,
+    detuning_frequency = detuning_frequency,
+    self_kerr_coefficient = self_kerr_coefficient
 )
 
-target = rand(4,2)
-pcof = rand(8)
 
-orders = [2, 4]
-cost_types = [:Infidelity, :Tracking, :Norm]
+D1 = 4
+carrier_wave_freqs = [-k*self_kerr_coefficient for k in 0:2]
+control = bspline_control(tf, D1, carrier_wave_freqs)
+
+N_tot_levels = N_ess + N_guard
+target = rand(2*N_tot_levels, N_ess)
+pcof = rand(control.N_coeff)
+
+orders = (2, 4)
+cost_types = (:Infidelity, :Tracking, :Norm)
+
+
+# Smoke test, just make sure the main functions run without throwing exceptions
+@testset "Gradient Calculations Run Without Throwing Exceptions" begin
+for order in orders
+    for cost_type in cost_types
+        eval_forward(prob, control, pcof, order=order)
+
+        discrete_adjoint(prob,            control, pcof, target, order=order, cost_type=cost_type)
+        eval_grad_finite_difference(prob, control, pcof, target, order=order, cost_type=cost_type)
+        eval_grad_forced(prob,            control, pcof, target, order=order, cost_type=cost_type)
+    end
+end
+@test true
+end
+
 
 @testset "Gradient Agreement Between Methods" begin
-    HermiteOptimalControl.test_gradient_agreement(
-        prob, control, pcof, target, orders=orders, cost_types=cost_types
-    )
+for order in orders
+    @testset "Order: $order" begin
+    for cost_type in cost_types
+        @testset "Cost Function: $cost_type" begin
+        HermiteOptimalControl.test_gradient_agreement(
+            prob, control, pcof, target, order=order, cost_type=cost_type,
+            print_results=true
+        )
+        end
+    end
+    end
+end
 end # @testset "Checking Gradient Agreement Between Methods"
 
 @testset "Covergence" begin
--- test/test_gradient.jl
+++ test/test_gradient.jl
@@ -1,71 +1,63 @@
 using Test: @test, @testset
 
 function test_gradient_agreement(prob, control, pcof, target; 
-        orders=[2, 4], cost_types=[:Infidelity, :Tracking, :Norm],
+        order=2, cost_type=:Infidelity,
         print_results=false
     )
 
-    grad_diff_norms = Array{Float64, 3}(undef, 2, length(orders), length(cost_types))
 
     # Check that gradients calculated using discrete adjoint and finite difference
     # methods agree to reasonable precision
-    @testset "Discrete Adjoint Vs Finite Difference Vs Forced Method" begin
-        for (order_ind, order) in enumerate(orders)
-        @testset "Order: $order" begin
-            for (cost_ind, cost_type) in enumerate(cost_types)
-            @testset "Cost function: $cost_type" begin
-                grad_disc_adj = discrete_adjoint(
-                    prob, control, pcof, target, order=order,
-                    cost_type=cost_type
-                )
+    grad_disc_adj = discrete_adjoint(
+        prob, control, pcof, target, order=order,
+        cost_type=cost_type
+    )
 
-                grad_forced = eval_grad_forced(
-                    prob, control, pcof, target, order=order,
-                    cost_type=cost_type
-                )
+    grad_forced = eval_grad_forced(
+        prob, control, pcof, target, order=order,
+        cost_type=cost_type
+    )
 
-                grad_fin_diff = eval_grad_finite_difference(
-                    prob, control, pcof, target, order=order,
-                    cost_type=cost_type
-                )
+    grad_fin_diff = eval_grad_finite_difference(
+        prob, control, pcof, target, order=order,
+        cost_type=cost_type
+    )
 
-                forced_atol = 1e-14
-                fin_diff_atol = 1e-9 # Might want to relax this to 1e-9.
+    forced_atol = 1e-14
+    fin_diff_atol = 1e-9
 
-                @testset "Forced Method" begin
-                    for k in 1:length(grad_disc_adj)
-                        @test isapprox(grad_disc_adj[k], grad_forced[k], atol=forced_atol, rtol=forced_atol)
-                    end
-                end
-
-                @testset "Finite Difference" begin
-                    for k in 1:length(grad_disc_adj)
-                        @test isapprox(grad_disc_adj[k], grad_fin_diff[k], atol=fin_diff_atol, rtol=forced_atol)
-                    end
-                end
-
-                disc_adj_minus_forced = grad_disc_adj - grad_forced
-                disc_adj_minus_fin_diff = grad_disc_adj - grad_fin_diff
-
-                grad_diff_norms[1, order_ind, cost_ind] = norm(disc_adj_minus_forced)
-                grad_diff_norms[2, order_ind, cost_ind] = norm(disc_adj_minus_fin_diff)
-
-                if print_results
-                    println("Order=$order, cost_type=$cost_type")
-                    println("--------------------------------------------------")
-                    println("Discrete Adjoint Gradient:\n", grad_disc_adj)
-                    println("\nForced Method Gradient:\n", grad_forced)
-                    println("\nFinite Difference Gradient:\n", grad_fin_diff)
-                    println("\n\nDiscrete Adjoint - Forced Method:\n", disc_adj_minus_forced)
-                    println("Discrete Adjoint - Finite Difference:\n", disc_adj_minus_fin_diff)
-                    println("\n\n")
-                end
-
-            end #@testset "Order: $order"
+    @testset "Forced Method" begin
+        for k in 1:length(grad_disc_adj)
+            @test isapprox(grad_disc_adj[k], grad_forced[k], atol=forced_atol, rtol=forced_atol)
         end
-        end #@testset "Cost function: $cost_type"
     end
-    end # @testset "Discrete Adjoint Vs Finite Difference Vs Forced Method"
 
-    return grad_diff_norms
+    @testset "Finite Difference" begin
+        for k in 1:length(grad_disc_adj)
+            @test isapprox(grad_disc_adj[k], grad_fin_diff[k], atol=fin_diff_atol, rtol=forced_atol)
+        end
+    end
+
+    disc_adj_minus_forced = grad_disc_adj - grad_forced
+    disc_adj_minus_fin_diff = grad_disc_adj - grad_fin_diff
+
+    
+    maximum(abs.(disc_adj_minus_fin_diff))
+
+
+    if print_results
+        println("Order=$order, cost_type=$cost_type")
+        println("--------------------------------------------------")
+        #println("Discrete Adjoint Gradient:\n", grad_disc_adj)
+        #println("\nForced Method Gradient:\n", grad_forced)
+        #println("\nFinite Difference Gradient:\n", grad_fin_diff)
+        #println("\n\nDiscrete Adjoint - Forced Method:\n", disc_adj_minus_forced)
+        #println("Discrete Adjoint - Finite Difference:\n", disc_adj_minus_fin_diff)
+        println("||Discrete Adjoint - Forced Method||∞: ", maximum(abs.(disc_adj_minus_forced)))
+        println("||Discrete Adjoint - Finite Difference||∞: ", maximum(abs.(disc_adj_minus_fin_diff)))
+        println("\n")
+    end
+
+
+    return nothing
 end
