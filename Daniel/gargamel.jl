cfb07595dff91996488ebd959d383965904af03e
Daniel Appelo <appelo@vt.edu>
2023-12-06 10:06:43 -0500

    Wednesday morning

--- Daniel/gargamel.jl
+++ Daniel/gargamel.jl
@@ -26,6 +26,8 @@ using HermiteOptimalControl
 using LinearMaps
 using IterativeSolvers
 using Plots
+using DifferentialEquations
+
 
 include("hermite_map.jl")
 
@@ -78,25 +80,15 @@ function Aw(w,H0,P,Q,p_tay,q_tay,m,c,dt)
 end
 
 
-function main(;d=3, N_guard=1, D1=missing, tf=missing, nsteps=missing)
+function main(nt,nt_ctrl,m ; d=3, N_guard=1)
 
     N_ess_levels = d+1
-    # Set up problem (for d = 1, 2, I just loosely followed pattern for nsteps and duration)
-
-    # If everything else is in GHz, then I think times should be in ns
-    duration_defaults = [50.0, 100.0, 140.0, 215.0, 265.0, 425.0]
-    if ismissing(tf)
-        tf = duration_defaults[d]
-    end
-    nsteps_defaults = [3500, 7000, 14787, 37843, 69962, 157082] # Defaults, as in Juqbox
-    if ismissing(nsteps)
-        nsteps = nsteps_defaults[d]
-    end
+    tf = 2.0*pi
+    nsteps = nt
 
     # Frequencies in GHz, *not angular*
     detuning_frequency = 0.0
-    self_kerr_coefficient =  0.1
-    
+    self_kerr_coefficient =  0.22
     prob = rotating_frame_qubit(
         N_ess_levels,
         N_guard,
@@ -106,35 +98,65 @@ function main(;d=3, N_guard=1, D1=missing, tf=missing, nsteps=missing)
         nsteps = nsteps
     )
     
-    m = 7
     H0 = [prob.system_asym prob.system_sym
           -prob.system_sym prob.system_asym]
     Q = [prob.asym_operators[1] 0*prob.asym_operators[1]
          0*prob.asym_operators[1] prob.asym_operators[1]]
     P = [0*prob.sym_operators[1] prob.sym_operators[1]
          -prob.sym_operators[1] 0*prob.sym_operators[1]]
+
+    n_w = 2*prob.N_tot_levels
+    tend = tf
+    dt = tend/nt
+    kk = 1
+    w0 = zeros(n_w)
+    w0[kk] = 1.0
+    
+
+    f(w, p, t) = H0*w .+ sin(t).*(P*w) .+ cos(t).*(Q*w)
+    tspan = (0.0, tend)
+    prob_DEQ = ODEProblem(f, w0, tspan)
+    sol = solve(prob_DEQ,Tsit5(), reltol = 1e-14, abstol = 1e-14)
+    pl0 = plot(sol)
     
     c = zeros(m+1)
     for j = 0:m
         c[1+j] = (factorial(m)*factorial(2*m-j))/(factorial(2*m)*factorial(m-j))
     end
-
-    n_w = 2*prob.N_tot_levels
-    tend = tf
-    nt = 15
-    dt = tend/nt
-    w0 = zeros(n_w)
-    w0[1] = 1.0
-    W = zeros(length(w0),m+1,nt+1)
-    T = LinRange(0,tend,nt+1)
-
-    nt_ctrl = 3
+    
     dt_ctrl = tend/nt_ctrl
     # These arrays hold the controls 
+    # For now we use the convention that these are Taylor coefficients that are
+    # scaled with dt_ctrl^k/k! just as in Chides
     P_tay = zeros(m+1,nt_ctrl+1)
     Q_tay = zeros(m+1,nt_ctrl+1)
-    P_tay[1,:] .= 0.22
 
+    #P_tay = randn(Float64,(m+1,nt_ctrl+1))
+    #Q_tay = randn(Float64,(m+1,nt_ctrl+1))
+
+    # we consider the taylor expansion of the functions
+    # p(t) = sin(t)
+    # q(t) = cos(t)
+    for it = 0:nt_ctrl
+        t = it*dt_ctrl
+        df = 1.0
+        for i = 0:m
+            if mod(i,4) == 0
+                Q_tay[1+i,1+it] =  df*cos(t)
+                P_tay[1+i,1+it] =  df*sin(t)
+            elseif mod(i,4) == 1
+                Q_tay[1+i,1+it] = -df*sin(t)
+                P_tay[1+i,1+it] =  df*cos(t)
+            elseif mod(i,4) == 2
+                Q_tay[1+i,1+it] = -df*cos(t)
+                P_tay[1+i,1+it] = -df*sin(t)
+            elseif mod(i,4) == 3
+                Q_tay[1+i,1+it] =  df*sin(t)
+                P_tay[1+i,1+it] = -df*cos(t)
+            end
+            df = df*dt_ctrl/(i+1)
+        end
+    end
     tloc_ctrl = zeros(nt+1)
     idx_ctrl = zeros(Int64,nt+1)
     # We find the map between the timestep index and the control array index
@@ -169,6 +191,9 @@ function main(;d=3, N_guard=1, D1=missing, tf=missing, nsteps=missing)
     Hmat = zeros(2*m+2,2*m+2)
     Hermite_map!(Hmat,m,0.0,1.0,0.5,0)
 
+    W = zeros(length(w0),m+1,nt+1)
+    T = LinRange(0,tend,nt+1)
+        
     q = 2*m+1
     ucof = zeros(2*m+2)
     it = 0
@@ -219,25 +244,50 @@ function main(;d=3, N_guard=1, D1=missing, tf=missing, nsteps=missing)
     end
 
     neval = 101
-    pl = plot(T,sqrt.(W[1,1,:].^2+W[6,1,:].^2),lw=2,marker=:star)
+    pl = plot(T,0.0 .* sqrt.(W[1,1,:].^2+W[6,1,:].^2),lw=2,marker=:star,label=:none)
     
 
     z = collect(LinRange(-0.5,0.5,neval))
+    for j = 1:prob.N_tot_levels
+        for i = 1:nt
+            ifield = j
+            ucof[1:m+1] = scale[1:m+1,1:m+1]*W[ifield,:,i]
+            ucof[m+2:2*m+2] = scale[1:m+1,1:m+1]*W[ifield,:,i+1]
+            uint = Hmat*ucof
+            uplot = evalhinterp(z,uint)
+            ifield = j+prob.N_tot_levels
+            ucof[1:m+1] = scale[1:m+1,1:m+1]*W[ifield,:,i]
+            ucof[m+2:2*m+2] = scale[1:m+1,1:m+1]*W[ifield,:,i+1]
+            uint = Hmat*ucof
+            vplot = evalhinterp(z,uint)
+            #plot!(pl,T[i] .+ dt*(0.5 .+ z),sqrt.(uplot.^2 .+ vplot.^2),color=:black,lw=1,label = :none)
+            plot!(pl,T[i] .+ dt*(0.5 .+ z),uplot,color=:black,lw=1,label = :none)
+            plot!(pl,T[i] .+ dt*(0.5 .+ z),vplot,color=:red,lw=1,label = :none)
+        end
+    end
+
+
+    pl1 = plot(sol.t,sol[1,:],color=:red,lw=2,label = :none)
+    j = 1
     for i = 1:nt
-        ifield = 1
+        ifield = j
         ucof[1:m+1] = scale[1:m+1,1:m+1]*W[ifield,:,i]
         ucof[m+2:2*m+2] = scale[1:m+1,1:m+1]*W[ifield,:,i+1]
         uint = Hmat*ucof
         uplot = evalhinterp(z,uint)
-        ifield = 6
-        ucof[1:m+1] = scale[1:m+1,1:m+1]*W[ifield,:,i]
-        ucof[m+2:2*m+2] = scale[1:m+1,1:m+1]*W[ifield,:,i+1]
-        uint = Hmat*ucof
-        vplot = evalhinterp(z,uint)
-        plot!(pl,T[i] .+ dt*(0.5 .+ z),sqrt.(uplot.^2 .+ vplot.^2),color=:black,lw=1,label = :none)
+        plot!(pl1,T[i] .+ dt*(0.5 .+ z),uplot,color=:black,lw=1,label = :none)
     end
-    return T,W,pl
+    maxerr = maximum(abs.(sol[:,end] .- W[:,1,end]))
+    return T,W,sol,pl,pl0,pl1,maxerr
 end
 
-T,W,pl = main()
-display(pl)
+function get_err(nt,nt_ctrl,m)
+    #nt = 50
+    #nt_ctrl = 2
+    #m = 1
+    T,W,sol,pl,pl0,pl1,maxerr = main(nt,nt_ctrl,m)
+    #pl = main()
+    display(pl1)
+    println("Maximum error is: ",maxerr)
+    return nothing
+end
