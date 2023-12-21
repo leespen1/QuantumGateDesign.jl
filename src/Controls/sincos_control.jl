b7d244aa36857b5db04cd43c6857c47e2a481641
Spencer Lee <leespen1@msu.edu>
2023-12-20 21:08:33 -0500

    Added amplitude control

--- src/Controls/sincos_control.jl
+++ src/Controls/sincos_control.jl
@@ -1,3 +1,7 @@
+"""
+Control with 2 coefficients. One to control amplitude of sin, one to control
+amplitude of cos.
+"""
 struct SinCosControl <: AbstractControl
     N_coeff::Int64
     tf::Float64
@@ -5,16 +9,16 @@ struct SinCosControl <: AbstractControl
     function SinCosControl(tf::Real, frequency::Real=1.0)
         tf_f64 = convert(Float64, tf)
         frequency_f64 = convert(Float64, frequency)
-        N_coeff = 0
+        N_coeff = 2
         new(N_coeff, tf_f64, frequency_f64)
     end
 end
 
 function eval_p(control::SinCosControl, t::Real, pcof::AbstractVector{<: Real})
-    return sin(t*control.frequency)
+    return sin(t*control.frequency)*pcof[1]
 end
 
 
 function eval_q(control::SinCosControl, t::Real, pcof::AbstractVector{<: Real})
-    return cos(t*control.frequency)
+    return cos(t*control.frequency)*pcof[2]
 end
