85dc952351175ba955de2316893cb617f18513c5
Spencer Lee <leespen1@msu.edu>
2024-02-06 15:12:30 -0500

    Updated control visualizer

--- ext/ControlVisualizer.jl
+++ ext/ControlVisualizer.jl
@@ -5,132 +5,161 @@ using GLMakie
 #using CairoMakie
 
 
-function HermiteOptimalControl.visualize_control(control, tf, npoints; prob=missing, 
-        pcof_init=missing, use_slidergrid=true, use_tboxes=true)
-    # Set up Makie figure and axis for plotting
+"""
+Visualize a Control Using Makie
+
+This is really old. From back when I was using functions as parameters for the
+object instead of methods for the type. Needs to be updated to work.
+"""
+function HermiteOptimalControl.visualize_control(control; n_points=101, prob=missing, 
+        pcof_init=missing, use_tboxes=true)
+
+    tf = control.tf
+
+    t_range_control = LinRange(0, tf, n_points)
+
+    # Set up Makie figure and axis for plotting the control
     fig = Figure(;)
-    ax = Axis(fig[1,1], xlabel="Time (ns)", ylabel="Control Amplitude (MHz)")
+    ax = Axis(fig[1,1], xlabel="Time (ns)", ylabel="Control Amplitude (Unitless)")
+    xlims!(ax, -(1/16)*tf, tf*(1+(1/16))) # Put a little bit of padding outside the control range
+
+    #==================================
+    # Set up control vector slidergrid
+    ==================================#
 
     # Set up control vector pcof, and slidergrid for manipulating control vector
-    pcof_o = Vector{Observable{Float64}}(undef, control.N_coeff)
+    pcof_obsv = Vector{Observable{Float64}}(undef, control.N_coeff)
 
+    # Set initial values for control vector. Initialize to user-specified values, if given
     startvalues = zeros(control.N_coeff)
     if !ismissing(pcof_init)
         startvalues .= pcof_init
     end
 
-    # Set up timing observables
-    n_points_o = Observable{Int64}(npoints)
-    tf_o = Observable{Float64}(tf)
+    #pcof_range = LinRange(0, 1e-3, 11)
+    pcof_range = LinRange(0, 1, 101)
 
-    # Time grid tracks final time
-    # Having the number of points in linrange seems to produce errors, but the plot still works
-    ts_o = lift((t,n) -> LinRange(0,t,n), tf_o, n_points_o)
-
-    pcof_range = LinRange(0, 1e-3, 10)
-
-    if use_slidergrid
-        pcof_slider_parameters = [
-            (label="pcof[$i]", range=pcof_range, startvalue=startvalues[i])
-            for i in 1:control.N_coeff
-        ]
-        pcof_slidergrid = SliderGrid(fig[4,1], pcof_slider_parameters...)
-
-        # Set up link between slider values and pcof observable values
+    pcof_slider_parameters = [
+        (label="pcof[$i]", range=pcof_range, startvalue=startvalues[i])
         for i in 1:control.N_coeff
-            pcof_o[i] = lift(x -> x, pcof_slidergrid.sliders[i].value)
-        end
-    elseif !ismissing(pcof_init)
-        for i in 1:control.N_coeff
-            pcof_o[i] = Observable{Float64}(pcof_init[i])
-        end
-    else
-        throw("use_slidergrid=false, but no initial control vector provided")
-    end
+    ]
 
+    pcof_slidergrid = SliderGrid(fig[2,1], pcof_slider_parameters...)
 
-
-    if use_tboxes
-        # Setup textbox for modification of final time, and number of grid points
-        tb_tf = Textbox(fig[2,1], placeholder= "Enter final time", validator=Float64, tellwidth=false)
-        on(tb_tf.stored_string) do s
-            tf_o[] = parse(Float64, s)
-        end
-
-        tb_npoints = Textbox(fig[3,1], placeholder= "Number of gridpoints", validator=Int64, tellwidth=false)
-        on(tb_npoints.stored_string) do s
-            n_points_o[] = parse(Int64, s)
-        end
+    # Set up link between slider values and pcof observable values
+    for i in 1:control.N_coeff
+        pcof_obsv[i] = lift(x -> x, pcof_slidergrid.sliders[i].value)
     end
 
     # Set up function value observables
-    p_vals_o = Observable(zeros(n_points_o[]))
-    q_vals_o = Observable(zeros(n_points_o[]))
+    p_vals_obsv = Observable([Point2(t_range_control[k], eval_p(control, t_range_control[k], startvalues)*1e3) for k in 1:n_points])
+    q_vals_obsv = Observable([Point2(t_range_control[k], eval_q(control, t_range_control[k], startvalues)*1e3) for k in 1:n_points])
 
-    # Set up function for updating values of p and q when coefficients or times change
-    # (in units of MHz, non angular)
+    # If prob is provided, also plot population evolution
+    if !ismissing(prob)
+        @assert tf == prob.tf # Make sure problem and control have same range
+        t_range_prob = LinRange(0, tf, 1+prob.nsteps)
+
+        # Set up plot for populations
+        ax_population = Axis(fig[3,1], xlabel="Time (ns)", ylabel="Populations")
+        xlims!(ax_population, -(1/16)*tf, tf*(1+(1/16)))
+
+        # Set up population value observables
+        # Each initial condition has it's own population array
+        populations_obsv_list = [Observable(zeros(size(prob.u0, 1), 1+prob.nsteps)) for i in 1:size(prob.u0, 2)]
+
+    end
+
+    #=================================================
+    # Function for updating graph if parameters change
+    =================================================#
     function update_graph()
-        pcof = to_value.(pcof_o)
-        p_vals_o[] = [control.p[1](ts_o[][k], pcof)*1e3 for k in 1:n_points_o[]]
-        q_vals_o[] = [control.q[1](ts_o[][k], pcof)*1e3 for k in 1:n_points_o[]]
+        pcof = to_value.(pcof_obsv)
+        # (in units of MHz, non angular)
+        p_vals_obsv[] = [Point2(t_range_control[k], eval_p(control, t_range_control[k], pcof)) for k in 1:n_points]
+        q_vals_obsv[] = [Point2(t_range_control[k], eval_q(control, t_range_control[k], pcof)) for k in 1:n_points]
+
+        max_p = maximum(getindex.(p_vals_obsv[], 2))
+        max_q = maximum(getindex.(q_vals_obsv[], 2))
+        min_p = minimum(getindex.(p_vals_obsv[], 2))
+        min_q = minimum(getindex.(q_vals_obsv[], 2))
 
         # Update ylims to capture entire control; const args ensure ylims != (0,0)
-        ylims!(ax, min(minimum(p_vals_o[]), minimum(q_vals_o[]))-1,
-                   max(maximum(p_vals_o[]), maximum(q_vals_o[]))+1)
+        ylims!(ax, min(min_p, min_q)-1, max(max_p, max_q)+1)
 
-        #=
         if !ismissing(prob)
             history = eval_forward(prob, control, pcof, order=4)
             populations = get_populations(history)
-            for i in 1:prob.N_ess_levels
-                populations_o[i][] = populations[i,:]
+            for i in 1:size(prob.u0, 2)
+                populations_obsv_list[i][] = populations[:,:,i]
             end
         end
-        =#
     end
 
     # Initial update
     update_graph()
 
-    # Set xlims to track final time (and leave a fraction of the window empty)
-    on(tf_o) do tf
-        xlims!(ax, -(1/16)*tf, tf*(1+(1/16)))
-        update_graph()
-    end
 
-    on(n_points_o) do n_points
-        update_graph()
-    end
-
-    # Set up population value observables
-    #=
-    if !ismissing(prob)
-        populations_o = [Observable(zeros(n_points_o[])) for i in 1:prob.N_ess_levels]
-    end
-    =#
-
-
-    # set p/q_vals to change if any of the coefficients change
+    # Set up link so that graph updates when any control parameter changes
     for i in 1:control.N_coeff
-        on(pcof_o[i]) do coeff
+        on(pcof_obsv[i]) do coeff
             update_graph()
         end
     end
 
+    # Draw the control functions
+    GLMakie.lines!(ax, p_vals_obsv, linewidth=2)
+    GLMakie.lines!(ax, q_vals_obsv, linewidth=2)
 
-    # Draw the functions
-    Makie.lines!(ax, ts_o, p_vals_o, linewidth=2)
-    Makie.lines!(ax, ts_o, q_vals_o, linewidth=2)
-    #=
+    # Draw the populations
     if !ismissing(prob)
-        for i in 1:prob.
-            Makie.lines!(ax, ts_o, populations_o[i])
+        #for i in 1:size(prob.u0, 2)
+        for i in 1:1
+            GLMakie.series!(ax_population, t_range_prob, populations_obsv_list[i])
         end
     end
-    =#
+
+
+    # For changing layouts. Ideally, I should have functions for constructing
+    # the various layouts, i.e. doing the actual plotting, and the body of this
+    # function is just to set up the observables.
+    #
+    # This will make it easier to manage each kind of graph I am showing. For example,
+    # suppose I want to show the cost function next to the population graph. Better to have
+    # that all in one function, rather than existing alongside the control plotting.
+    options = ["Graph 1", "Graph 2", "Graph 3"]
+    menu = Menu(fig[4,1], options=options)
+
 
     return fig
 end
 
+"""
+Get (approximately) the maximum control amplitude.
+
+I am assuming that the control amplitude generally goes up as the control
+parameters increase in value.
+
+I am only using this function to help set the axis limits in the control
+visualization, so it is fine if this is not very accurate.
+"""
+function get_max_control_amp(control, n_samples, pcof_upper_lim, pcof_lower_lim)
+    pcof_upper = ones(control.N_coeff) .* pcof_upper_lim
+    pcof_lower = ones(control.N_coeff) .* pcof_lower_lim
+
+    ts = LinRange(0.0, control.tf, n_samples)
+
+    p_vals_upper = [eval_p(control, t, pcof_upper) for t in ts]
+    q_vals_upper = [eval_q(control, t, pcof_upper) for t in ts]
+
+    p_vals_lower = [eval_p(control, t, pcof_lower) for t in ts]
+    q_vals_lower = [eval_q(control, t, pcof_lower) for t in ts]
+
+    control_val_lists = [p_vals_upper, q_vals_upper, p_vals_lower, q_vals_lower]
+    max_control = maximum()
+
+    
+    max_q = maximum(p_vals)
+end
 
 end # Module
