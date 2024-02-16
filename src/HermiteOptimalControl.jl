9feb99e79174db3b7500e9155b570f69aa88fe12
Spencer Lee <leespen1@msu.edu>
2024-02-16 12:55:47 -0500

    Added BenchmarkTools

--- src/HermiteOptimalControl.jl
+++ src/HermiteOptimalControl.jl
@@ -1,7 +1,7 @@
 module HermiteOptimalControl
 
 
-import LinearMaps, IterativeSolvers, Plots, Ipopt, ForwardDiff, LinearAlgebra
+import LinearMaps, IterativeSolvers, Plots, Ipopt, ForwardDiff, LinearAlgebra, BenchmarkTools
 using LinearAlgebra: mul!, axpy!, dot, tr, norm
 
 # Export derivative computation functions
