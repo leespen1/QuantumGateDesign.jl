1f8dd6294102a4e7adb66c9b9b3e7a54b17887c7
Spencer Lee <leespen1@msu.edu>
2024-02-16 11:56:03 -0500

    Changed some of the tests

--- src/Tests/test_convergence.jl
+++ src/Tests/test_convergence.jl
@@ -15,7 +15,7 @@ function plot_history_convergence(prob, control, pcof, N_iterations;
         orders=(2, 4, 6, 8), nsteps_change_factor=2,
         duration_error=true,
         include_orderlines=true, fontsize=16, true_history=missing,
-        error_limit=1e-15
+        error_limit=1e-15, n_runs=1
     )
     base_nsteps = prob.nsteps
 
@@ -49,6 +49,11 @@ function plot_history_convergence(prob, control, pcof, N_iterations;
 
     errors_all = Matrix{Float64}(undef, N_iterations, length(orders))
     timing_all = Matrix{Float64}(undef, N_iterations, length(orders))
+    timing_stddev_all = Matrix{Float64}(undef, N_iterations, length(orders))
+    # Fill with NaN, so that we can break the loop early and still plot, ignoring unfilled values
+    errors_all .= NaN
+    timing_all .= NaN
+    timing_stddev_all .= NaN
 
     step_sizes = (prob.tf/base_nsteps) ./ [nsteps_change_factor^k for k in 0:N_iterations-1]
 
@@ -59,10 +64,11 @@ function plot_history_convergence(prob, control, pcof, N_iterations;
     for (j, order) in enumerate(orders)
         errors = Vector{Float64}(undef, N_iterations)
         timing = Vector{Float64}(undef, N_iterations)
-
-        # Fill with NaN, so that we can break the loop early and still plot, ignoring unfilled values
+        timing_stddev = Vector{Float64}(undef, N_iterations)
         errors .= NaN
         timing .= NaN
+        timing_stddev .= NaN
+
 
         N_derivatives = div(order, 2)
 
@@ -73,8 +79,16 @@ function plot_history_convergence(prob, control, pcof, N_iterations;
             nsteps_multiplier = nsteps_change_factor^(k-1)
             prob_copy.nsteps = base_nsteps*nsteps_multiplier
 
-            history = zeros(prob.real_system_size, N_derivatives+1, 1+prob_copy.nsteps, N_initial_conditions)
-            elapsed_time = @elapsed eval_forward!(history, prob_copy, control, pcof, order=order)
+            elapsed_times = zeros(n_runs)
+            for rerun_i in 1:n_runs
+                elapsed_times[rerun_i] = @elapsed history = eval_forward(prob_copy, control, pcof, order=order)
+            end
+            mean_elapsed_time = sum(elapsed_times)/length(elapsed_times)
+            stddev_elapsed_time = sum((elapsed_times .- mean_elapsed_time) .^ 2)
+            stddev_elapsed_time /= length(elapsed_times)-1
+            stddev_elapsed_time = sqrt(stddev_elapsed_time)
+
+
             # Skip over steps to match base_nsteps solution
             history = history[:,1,1:nsteps_multiplier:end,:]
 
@@ -85,12 +99,14 @@ function plot_history_convergence(prob, control, pcof, N_iterations;
             end
 
             errors[k] = error
-            timing[k] = elapsed_time
+            timing[k] = mean_elapsed_time
+            timing_stddev[k] = stddev_elapsed_time
 
-            println("----------------------------------------")
             println("Nsteps = ", prob_copy.nsteps)
             println("Error = ", error)
-            println("Elapsed Time = ", elapsed_time)
+            println("Mean Elapsed Time = ", mean_elapsed_time)
+            println("StdDev Elapsed Time = ", stddev_elapsed_time)
+            println("----------------------------------------")
 
             # Break once we reach high enough precision
             if error < error_limit 
@@ -104,13 +120,13 @@ function plot_history_convergence(prob, control, pcof, N_iterations;
 
         errors_all[:,j] .= errors
         timing_all[:,j] .= timing
+        timing_stddev_all[:,j] .= timing_stddev
         #errors_all[:,length(orders)+j] .= order_line
     end
 
     stepsize_xlims = collect(Plots.xlims(pl_stepsize))
     timing_xlims   = collect(Plots.xlims(pl_timing))
     Plots.plot!(pl_stepsize, stepsize_xlims, [-7, -7], linecolor=:red, label="Target Error")
-    Plots.plot!(pl_timing,   timing_xlims,   [-7, -7], linecolor=:red, label="Target Error")
     # If interrupted by keyboard, stop early but still finish the graph
     catch e
         if e isa InterruptException
@@ -146,26 +162,62 @@ function plot_history_convergence(prob, control, pcof, N_iterations;
         Plots.plot!(pl_stepsize, legend=:outerright)
     end
 
-    return pl_stepsize, pl_timing, step_sizes, errors_all, timing_all
+    return pl_stepsize, pl_timing, step_sizes, errors_all, timing_all, timing_stddev_all
 end
-#=
-detuning_frequency    = 1.0
-self_kerr_coefficient = 0.5
-N_ess_levels = 2
-N_guard_levels = 0
-tf = 1.0
-nsteps = 5
-prob = HermiteOptimalControl.single_transmon_qubit_rwa(
-    detuning_frequency, self_kerr_coefficient, N_ess_levels,
-    N_guard_levels, tf, nsteps
-)
 
-N_coeff_per_control = 4
-control = HermiteOptimalControl.sincos_control(4)
+function just_plot_history_convergence(step_sizes, errors_all, timing_all, timing_stddev_all;
+        orders=(2, 4, 6, 8, 10), include_orderlines=true, fontsize=16, orderline_offset=0
+    )
 
-pcof = ones(8)
+    pl_stepsize = Plots.plot(ylabel="Log₁₀(Rel Err)", fontsize=fontsize)
+    pl_timing = Plots.plot(ylabel="Log₁₀(Rel Err)", fontsize=fontsize)
+    Plots.plot!(pl_stepsize, xlabel="Log₁₀(Step Size Δt)")
+    Plots.plot!(pl_timing, xlabel="Log₁₀(Elapsed Time) (s)")
 
-N_iterations = 13
+    yticks = -15:15 
+    Plots.plot!(pl_stepsize, yticks=yticks, legend=:outerright)
+    Plots.plot!(pl_timing, yticks=yticks, legend=:outerright)
 
-pl = HermiteOptimalControl.plot_history_convergence(prob, control, pcof, N_iterations)
-=#
+    labels = ["Order=$order" for order in orders]
+    labels = reshape(labels, 1, :)
+    Plots.plot!(pl_timing, log10.(timing_all), log10.(errors_all), marker=:circle, markersize=5, labels=labels, linealpha=0.5)
+    Plots.plot!(pl_stepsize, log10.(step_sizes), log10.(errors_all), marker=:circle, markersize=5, labels=labels)
+
+    stepsize_xlims = collect(Plots.xlims(pl_stepsize))
+    timing_xlims   = collect(Plots.xlims(pl_timing))
+    Plots.plot!(pl_stepsize, stepsize_xlims, [-7, -7], linecolor=:red, label="Target Error")
+
+    # Add order lines
+    if include_orderlines
+        # If a single number is given, fill vector with that number
+        if length(orderline_offset) == 1
+            orderline_offset = ones(length(orders)) .* orderline_offset
+        end
+        @assert length(orderline_offset) == length(orders)
+
+        # Order lines may extend too far down. Save the old limits so I can fix the window size back.
+        old_ylims = Plots.ylims(pl_stepsize)
+
+        linestyle_list = (:solid, :dash, :dot, :dashdot)
+        for (j, order) in enumerate(orders)
+            order_line = step_sizes .^ order
+            order_line .*= 2 * errors_all[1,j]/order_line[1] # Adjust vertical position to match data, with small offset for visibility
+
+            linestyle_index = j % length(linestyle_list)
+            if linestyle_index > 0
+                linestyle = linestyle_list[linestyle_index]
+            else 
+                linestyle = linestyle_list[end]
+            end
+
+            Plots.plot!(
+                pl_stepsize, log10.(step_sizes), log10.(order_line) .+ orderline_offset[j],
+                label="Δt^$order", linecolor=:black, linestyle=linestyle
+            )
+        end
+
+        Plots.ylims!(pl_stepsize, old_ylims...)
+        Plots.plot!(pl_stepsize, legend=:outerright)
+    end
+    return pl_stepsize, pl_timing
+end
