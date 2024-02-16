81c5bf981172b1c69146ee9df381b6cf9e62a7c7
Spencer Lee <leespen1@msu.edu>
2024-02-16 12:56:57 -0500

    Changed control units to MHz

--- src/Plotting/plot_control.jl
+++ src/Plotting/plot_control.jl
@@ -3,13 +3,13 @@ function plot_control(control::AbstractControl, pcof::AbstractVector{Float64}; n
     p_control_vals = zeros(npoints)
     q_control_vals = zeros(npoints)
     for (i, t) in enumerate(t_grid)
-        p_control_vals[i] = eval_p_derivative(control, t, pcof, derivative_order)
-        q_control_vals[i] = eval_q_derivative(control, t, pcof, derivative_order)
+        p_control_vals[i] = eval_p_derivative(control, t, pcof, derivative_order) * 1e3/(2pi)
+        q_control_vals[i] = eval_q_derivative(control, t, pcof, derivative_order) * 1e3/(2pi)
     end
 
-    pl = Plots.plot(t_grid, p_control_vals, label="p(t) [d$derivative_order]")
-    Plots.plot!(t_grid, q_control_vals, label="q(t) [d$derivative_order]")
-    Plots.plot!(ylabel="Amplitude", xlabel="t")
+    pl = Plots.plot(t_grid, p_control_vals, label="p(t)")
+    Plots.plot!(t_grid, q_control_vals, label="q(t)")
+    Plots.plot!(ylabel="Amplitude/2π (MHz)", xlabel="Time (ns)")
 
     return pl
 end
