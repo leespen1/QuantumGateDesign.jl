68d0ac95d32644b9f7cb0e9916c400c8312eaad3
Spencer Lee <leespen1@msu.edu>
2024-03-07 17:27:15 -0500

    Separated data acquisition and plotting into multiple functions, so I can combine the results with Juqbox better

--- src/Tests/test_convergence.jl
+++ src/Tests/test_convergence.jl
@@ -11,7 +11,7 @@ of points in time when using the fewest steps / largest step size.
 
 Note: Plots.scalefontsizes(1.5-2.0) seems appropriate for slideshow
 """
-function plot_history_convergence(prob, control, pcof, N_iterations;
+function get_history_convergence(prob, control, pcof, N_iterations;
         orders=(2, 4, 6, 8), nsteps_change_factor=2,
         duration_error=true,
         include_orderlines=true, fontsize=16, true_history=missing,
@@ -23,19 +23,9 @@ function plot_history_convergence(prob, control, pcof, N_iterations;
     prob_copy = copy(prob)
     histories = Vector[]
 
-    pl_stepsize = Plots.plot(ylabel="Log₁₀(Rel Err)", fontsize=fontsize)
-    pl_timing = Plots.plot(ylabel="Log₁₀(Rel Err)", fontsize=fontsize)
-    Plots.plot!(pl_stepsize, xlabel="Log₁₀(Step Size Δt)")
-    Plots.plot!(pl_timing, xlabel="Log₁₀(Elapsed Time) (s)")
-
-    #yticks = [10.0 ^ n for n in -15:15] # Log10 scale version
-    yticks = -15:15 
-    Plots.plot!(pl_stepsize, yticks=yticks, legend=:outerright)
-    Plots.plot!(pl_timing, yticks=yticks, legend=:outerright)
-
     # Get "true" solution using many timesteps with highest order method
     if ismissing(true_history)
-        println("True history not provided, using 'fine-grain' numerical solution")
+        println("True history not provided, calculating 'fine-grain' numerical solution")
         most_steps = base_nsteps*(nsteps_change_factor ^ N_iterations)
         prob_copy.nsteps = most_steps
         true_history = eval_forward(
@@ -115,18 +105,12 @@ function plot_history_convergence(prob, control, pcof, N_iterations;
         end
 
 
-        Plots.scatter!(pl_timing, log10.(timing), log10.(errors), marker=:circle, markersize=5, label="Order=$order")
-        Plots.plot!(pl_stepsize, log10.(step_sizes), log10.(errors), marker=:circle, markersize=5, label="Order=$order")
-
         errors_all[:,j] .= errors
         timing_all[:,j] .= timing
         timing_stddev_all[:,j] .= timing_stddev
         #errors_all[:,length(orders)+j] .= order_line
     end
 
-    stepsize_xlims = collect(Plots.xlims(pl_stepsize))
-    timing_xlims   = collect(Plots.xlims(pl_timing))
-    Plots.plot!(pl_stepsize, stepsize_xlims, [-7, -7], linecolor=:red, label="Target Error")
     # If interrupted by keyboard, stop early but still finish the graph
     catch e
         if e isa InterruptException
@@ -137,93 +121,12 @@ function plot_history_convergence(prob, control, pcof, N_iterations;
         end
     end
 
-
-    # Add order lines
-    if include_orderlines
-        # Order lines may extend too far down. Save the old limits so I can fix the window size back.
-        old_ylims = Plots.ylims(pl_stepsize)
-
-        linestyle_list = (:solid, :dash, :dot, :dashdot)
-        for (j, order) in enumerate(orders)
-            order_line = step_sizes .^ order
-            order_line .*= 2 * errors_all[1,j]/order_line[1] # Adjust vertical position to match data, with small offset for visibility
-
-            linestyle_index = j % length(linestyle_list)
-            if linestyle_index > 0
-                linestyle = linestyle_list[linestyle_index]
-            else 
-                linestyle = linestyle_list[end]
-            end
-
-            Plots.plot!(pl_stepsize, log10.(step_sizes), log10.(order_line), label="Δt^$order", linecolor=:black, linestyle=linestyle)
-        end
-
-        Plots.ylims!(pl_stepsize, old_ylims...)
-        Plots.plot!(pl_stepsize, legend=:outerright)
-    end
-
-    return pl_stepsize, pl_timing, step_sizes, errors_all, timing_all, timing_stddev_all
-end
-
-function just_plot_history_convergence(step_sizes, errors_all, timing_all, timing_stddev_all;
-        orders=(2, 4, 6, 8, 10), include_orderlines=true, fontsize=16, orderline_offset=0
-    )
-
-    pl_stepsize = Plots.plot(ylabel="Log₁₀(Rel Err)", fontsize=fontsize)
-    pl_timing = Plots.plot(ylabel="Log₁₀(Rel Err)", fontsize=fontsize)
-    Plots.plot!(pl_stepsize, xlabel="Log₁₀(Step Size Δt)")
-    Plots.plot!(pl_timing, xlabel="Log₁₀(Elapsed Time) (s)")
-
-    yticks = -15:15 
-    Plots.plot!(pl_stepsize, yticks=yticks, legend=:outerright)
-    Plots.plot!(pl_timing, yticks=yticks, legend=:outerright)
-
-    labels = ["Order=$order" for order in orders]
-    labels = reshape(labels, 1, :)
-    Plots.plot!(pl_timing, log10.(timing_all), log10.(errors_all), marker=:circle, markersize=5, labels=labels, linealpha=0.5)
-    Plots.plot!(pl_stepsize, log10.(step_sizes), log10.(errors_all), marker=:circle, markersize=5, labels=labels)
-
-    stepsize_xlims = collect(Plots.xlims(pl_stepsize))
-    timing_xlims   = collect(Plots.xlims(pl_timing))
-    Plots.plot!(pl_stepsize, stepsize_xlims, [-7, -7], linecolor=:red, label="Target Error")
-
-    # Add order lines
-    if include_orderlines
-        # If a single number is given, fill vector with that number
-        if length(orderline_offset) == 1
-            orderline_offset = ones(length(orders)) .* orderline_offset
-        end
-        @assert length(orderline_offset) == length(orders)
-
-        # Order lines may extend too far down. Save the old limits so I can fix the window size back.
-        old_ylims = Plots.ylims(pl_stepsize)
-
-        linestyle_list = (:solid, :dash, :dot, :dashdot)
-        for (j, order) in enumerate(orders)
-            order_line = step_sizes .^ order
-            order_line .*= 2 * errors_all[1,j]/order_line[1] # Adjust vertical position to match data, with small offset for visibility
-
-            linestyle_index = j % length(linestyle_list)
-            if linestyle_index > 0
-                linestyle = linestyle_list[linestyle_index]
-            else 
-                linestyle = linestyle_list[end]
-            end
-
-            Plots.plot!(
-                pl_stepsize, log10.(step_sizes), log10.(order_line) .+ orderline_offset[j],
-                label="Δt^$order", linecolor=:black, linestyle=linestyle
-            )
-        end
-
-        Plots.ylims!(pl_stepsize, old_ylims...)
-        Plots.plot!(pl_stepsize, legend=:outerright)
-    end
-    return pl_stepsize, pl_timing
+    return step_sizes, errors_all, timing_all, timing_stddev_all
 end
 
 
-function plot_history_convergence_juqbox(prob, pcof, wa, N_iterations;
+
+function get_history_convergence_juqbox(prob, pcof, wa, N_iterations;
         orders=(2,), nsteps_change_factor=2,
         duration_error=true,
         include_orderlines=true, fontsize=16, true_history=missing,
@@ -232,16 +135,6 @@ function plot_history_convergence_juqbox(prob, pcof, wa, N_iterations;
     base_nsteps = prob.nsteps
 
     histories = Vector[]
-
-    pl_stepsize = Plots.plot(ylabel="Log₁₀(Rel Err)", fontsize=fontsize)
-    pl_timing = Plots.plot(ylabel="Log₁₀(Rel Err)", fontsize=fontsize)
-    Plots.plot!(pl_stepsize, xlabel="Log₁₀(Step Size Δt)")
-    Plots.plot!(pl_timing, xlabel="Log₁₀(Elapsed Time) (s)")
-
-    #yticks = [10.0 ^ n for n in -15:15] # Log10 scale version
-    yticks = -15:15 
-    Plots.plot!(pl_stepsize, yticks=yticks, legend=:outerright)
-    Plots.plot!(pl_timing, yticks=yticks, legend=:outerright)
 
     # Get "true" solution using many timesteps with highest order method
     if ismissing(true_history)
@@ -265,7 +158,6 @@ function plot_history_convergence_juqbox(prob, pcof, wa, N_iterations;
     timing_stddev_all .= NaN
 
     step_sizes = (prob.T/base_nsteps) ./ [nsteps_change_factor^k for k in 0:N_iterations-1]
-
 
     N_initial_conditions = size(prob.Uinit, 2)
 
@@ -325,18 +217,12 @@ function plot_history_convergence_juqbox(prob, pcof, wa, N_iterations;
         end
 
 
-        Plots.scatter!(pl_timing, log10.(timing), log10.(errors), marker=:circle, markersize=5, label="Order=$order")
-        Plots.plot!(pl_stepsize, log10.(step_sizes), log10.(errors), marker=:circle, markersize=5, label="Order=$order")
-
         errors_all[:,j] .= errors
         timing_all[:,j] .= timing
         timing_stddev_all[:,j] .= timing_stddev
         #errors_all[:,length(orders)+j] .= order_line
     end
 
-    stepsize_xlims = collect(Plots.xlims(pl_stepsize))
-    timing_xlims   = collect(Plots.xlims(pl_timing))
-    Plots.plot!(pl_stepsize, stepsize_xlims, [-7, -7], linecolor=:red, label="Target Error")
     # If interrupted by keyboard, stop early but still finish the graph
     catch e
         if e isa InterruptException
@@ -347,31 +233,88 @@ function plot_history_convergence_juqbox(prob, pcof, wa, N_iterations;
         end
     end
 
+    prob.nsteps = base_nsteps # Return step sizes to original
+
+    return step_sizes, errors_all, timing_all, timing_stddev_all
+end
+
+
+
+
+function plot_history_convergence(step_sizes, errors_all, timing_all, timing_stddev_all; fontsize=16, kwargs...) 
+    
+    pl_stepsize = Plots.plot(ylabel="Log₁₀(Rel Err)", fontsize=fontsize)
+    pl_timing = Plots.plot(ylabel="Log₁₀(Rel Err)", fontsize=fontsize)
+    Plots.plot!(pl_stepsize, xlabel="Log₁₀(Step Size Δt)")
+    Plots.plot!(pl_timing, xlabel="Log₁₀(Elapsed Time) (s)")
+
+    yticks = -15:15 
+    Plots.plot!(pl_stepsize, yticks=yticks, legend=:outerright)
+    Plots.plot!(pl_timing, yticks=yticks, legend=:outerright)
+
+    plot_history_convergence!(pl_stepsize, pl_timing,
+        step_sizes, errors_all, timing_all, timing_stddev_all;
+        kwargs...
+    )
+    # Add the lines here
+
+    stepsize_xlims = collect(Plots.xlims(pl_stepsize))
+    timing_xlims   = collect(Plots.xlims(pl_timing))
+    Plots.plot!(pl_stepsize, stepsize_xlims, [-7, -7], linecolor=:red, label="Target Error")
+
+    return pl_stepsize, pl_timing
+end
+
+function plot_history_convergence!(pl_stepsize, pl_timing, step_sizes, errors_all, timing_all, timing_stddev_all;
+        orders=(2, 4, 6, 8, 10), include_orderlines=true, fontsize=16, orderline_offset=0,
+        labels=missing
+    )
+
+    N_orders = size(step_sizes, 2)
+
+    if ismissing(labels)
+        labels = ["Order=$order" for order in orders]
+        labels = reshape(labels, 1, :) # Labels must be row matrix
+    end
+
+    Plots.plot!(pl_timing, log10.(timing_all), log10.(errors_all), marker=:circle, markersize=5, labels=labels, linealpha=0.5)
+    Plots.plot!(pl_stepsize, log10.(step_sizes), log10.(errors_all), marker=:circle, markersize=5, labels=labels)
+
 
     # Add order lines
     if include_orderlines
+        # If a single number is given, fill vector with that number
+        if length(orderline_offset) == 1
+            orderline_offset = ones(length(orders)) .* orderline_offset
+        end
+        @assert length(orderline_offset) == length(orders)
+
         # Order lines may extend too far down. Save the old limits so I can fix the window size back.
         old_ylims = Plots.ylims(pl_stepsize)
 
         linestyle_list = (:solid, :dash, :dot, :dashdot)
-        for (j, order) in enumerate(orders)
+        for n in 1:N_orders
+            order = orders[n]
             order_line = step_sizes .^ order
-            order_line .*= 2 * errors_all[1,j]/order_line[1] # Adjust vertical position to match data, with small offset for visibility
+            order_line .*= 2 * errors_all[1,n]/order_line[1] # Adjust vertical position to match data, with small offset for visibility
 
-            linestyle_index = j % length(linestyle_list)
+            linestyle_index = n % length(linestyle_list)
             if linestyle_index > 0
                 linestyle = linestyle_list[linestyle_index]
             else 
                 linestyle = linestyle_list[end]
             end
 
-            Plots.plot!(pl_stepsize, log10.(step_sizes), log10.(order_line), label="Δt^$order", linecolor=:black, linestyle=linestyle)
+            Plots.plot!(
+                pl_stepsize, log10.(step_sizes), log10.(order_line) .+ orderline_offset[n],
+                label="Δt^$order", linecolor=:black, linestyle=linestyle
+            )
         end
 
         Plots.ylims!(pl_stepsize, old_ylims...)
         Plots.plot!(pl_stepsize, legend=:outerright)
     end
-
-    prob.nsteps = base_nsteps
-    return pl_stepsize, pl_timing, step_sizes, errors_all, timing_all, timing_stddev_all
+    return pl_stepsize, pl_timing
 end
+
+
