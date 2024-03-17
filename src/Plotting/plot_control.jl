e450c89ea357ae7340e9292593df208d8be728b2
Spencer Lee <leespen1@msu.edu>
2024-03-17 15:55:18 -0400

    Made unit conversion optional

--- src/Plotting/plot_control.jl
+++ src/Plotting/plot_control.jl
@@ -1,15 +1,23 @@
-function plot_control(control::AbstractControl, pcof::AbstractVector{Float64}; npoints=1001, derivative_order=0)
+function plot_control(control::AbstractControl, pcof::AbstractVector{Float64};
+        npoints=1001, derivative_order=0, convert_units=false)
     t_grid = LinRange(0, control.tf, npoints)
     p_control_vals = zeros(npoints)
     q_control_vals = zeros(npoints)
     for (i, t) in enumerate(t_grid)
-        p_control_vals[i] = eval_p_derivative(control, t, pcof, derivative_order) * 1e3/(2pi)
-        q_control_vals[i] = eval_q_derivative(control, t, pcof, derivative_order) * 1e3/(2pi)
+        p_control_vals[i] = eval_p_derivative(control, t, pcof, derivative_order)
+        q_control_vals[i] = eval_q_derivative(control, t, pcof, derivative_order)
     end
-
+    if convert_units
+        p_control_vals .*= 1e3/(2pi)
+        q_control_vals .*= 1e3/(2pi)
+    end
     pl = Plots.plot(t_grid, p_control_vals, label="p(t)")
     Plots.plot!(t_grid, q_control_vals, label="q(t)")
-    Plots.plot!(ylabel="Amplitude/2π (MHz)", xlabel="Time (ns)")
+    Plots.plot!(xlabel="Time (ns)")
+
+    if convert_units
+        Plots.plot!(ylabel="Amplitude/2π (MHz)")
+    end
 
     return pl
 end
