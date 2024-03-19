89898ed0b9d078749bd8c89a3db3fc1b12ab11b2
Spencer Lee <leespen1@msu.edu>
2024-03-19 19:30:44 -0400

    Changed some of the plotting options

--- src/Plotting/plot_control.jl
+++ src/Plotting/plot_control.jl
@@ -1,5 +1,5 @@
 function plot_control(control::AbstractControl, pcof::AbstractVector{Float64};
-        npoints=1001, derivative_order=0, convert_units=false)
+        npoints=1001, derivative_order=0, convert_units=false, linewidth=2)
     t_grid = LinRange(0, control.tf, npoints)
     p_control_vals = zeros(npoints)
     q_control_vals = zeros(npoints)
@@ -11,8 +11,8 @@ function plot_control(control::AbstractControl, pcof::AbstractVector{Float64};
         p_control_vals .*= 1e3/(2pi)
         q_control_vals .*= 1e3/(2pi)
     end
-    pl = Plots.plot(t_grid, p_control_vals, label="p(t)")
-    Plots.plot!(t_grid, q_control_vals, label="q(t)")
+    pl = Plots.plot(t_grid, p_control_vals, label="p(t)", lw=linewidth)
+    Plots.plot!(t_grid, q_control_vals, label="q(t)", lw=linewidth)
     Plots.plot!(xlabel="Time (ns)")
 
     if convert_units
