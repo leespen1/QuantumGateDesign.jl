2fbd65746d5538fbec44c9af1badb449ecdb0c38
Spencer Lee <leespen1@msu.edu>
2024-03-26 18:21:19 -0400

    Updated visualization examples

--- examples/QuIC/swap_visualization.jl
+++ examples/QuIC/swap_visualization.jl
@@ -6,8 +6,7 @@ prob = QuantumGateDesign.rotating_frame_qubit(2, 0, tf=10.0, nsteps=1000,
                                               self_kerr_coefficient=0.0)
 
 control = QuantumGateDesign.GRAPEControl(1, prob.tf)
-controls = [control, control]
 
 target = [0.0 1; 1 0; 0 0; 0 0]
 
-visualize_control(controls, prob=prob, target=target)
+visualize_control(control, prob=prob, target=target)
