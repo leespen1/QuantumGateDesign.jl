26331825ceabfef6541fc571681f867530ae09d4
Spencer Lee <leespen1@msu.edu>
2024-04-14 15:08:37 -0400

    updated indexing

--- test/hermite_interpolant.jl
+++ test/hermite_interpolant.jl
@@ -1,20 +1,23 @@
-using HermiteOptimalControl: HermiteControl
+using QuantumGateDesign: HermiteControl
 using Test: @test, @testset
 
 function test_hermite_interpolant(control::HermiteControl, pcof::AbstractVector{Float64})
-    q_offset = control.N_points*(1+control.N_derivatives)
+    q_offset = div(control.N_coeff, 2)
     rtol = 1e-14
 
-    @testset "Checking Hermite Interpolant at pcof points" begin
-    for i in 0:control.N_points-1
-        t = control.dt*i
-        for derivative_order in 0:control.N_derivatives
+    @test control.scaling_type == :Derivative
+    @testset "Checking Hermite Interpolant at control points" begin
+    for derivative_order in 0:control.N_derivatives
+        @testset "Order $derivative_order values" begin
+        for i in 0:control.N_points-1
+            t = control.dt*i
             p_val = eval_p_derivative(control, t, pcof, derivative_order)
-            q_val = eval_p_derivative(control, t, pcof, derivative_order)
+            q_val = eval_q_derivative(control, t, pcof, derivative_order)
 
             @test isapprox(p_val, pcof[1 + i*(1+control.N_derivatives) + derivative_order], rtol=rtol)
             @test isapprox(q_val, pcof[q_offset + 1 + i*(1+control.N_derivatives) + derivative_order], rtol=rtol)
         end
+        end  # @testset
     end
-    end # testset
+    end # @testset
 end
