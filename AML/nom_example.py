f89adbce9042ebc055bd896b0af146f21bccd33a
Spencer Lee <leespen1@msu.edu>
2024-04-16 02:10:31 -0400

    Got NOM working for a simple example (and with no fixed inputs, e.g. system physical parameters)

--- AML/nom_example.py
+++ AML/nom_example.py
@@ -11,12 +11,6 @@ def objective_function_model():
     ])
     return model
 
-# Create the model
-objective_model = objective_function_model()
-
-# Compile the model
-objective_model.compile(optimizer='adam', loss='mse')
-
 
 def objective_function_analytic(x):
     return (x-2)**2
@@ -44,7 +38,7 @@ objective_model = objective_function_model()
 objective_model.compile(optimizer='adam', loss='mse')
 
 # Train the model
-objective_model.fit(x_train, y_train, epochs=50, batch_size=32)
+objective_model.fit(x_train, y_train, epochs=5, batch_size=32)
 
 objective_model.save_weights('my_weights.weights.h5')
 
@@ -57,4 +51,64 @@ plt.scatter(x_test, y_pred, label="NN pred")
 plt.legend()
 
 plt.show()
+
+"""
+A layer that does nothing but add biases to the input. In this way, we 
+"""
+class StartingPointLayer(tf.keras.layers.Layer):
+    """
+    Use base class constructor
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def build(self, input_shape):
+        self.kernel = self.add_weight(
+            shape=(input_shape[-1],),
+            initializer="ones",
+            trainable=True,
+            name="kernel",
+        )
+
+        self.bias = self.add_weight(
+            shape=(input_shape[-1],),
+            initializer="zeros",
+            trainable=True,
+            name="bias",
+        )
+
+    """
+    Apply weights elementwise to inputs, and add biases
+    """
+    def call(self, inputs):
+        return tf.multiply(inputs, self.kernel) + self.bias
+
+
+nom_layer = NomLayer(input_shape=(1,))
+
+for layer in objective_model.layers:
+    layer.trainable = False
+
+print("Weights before optimization")
+print(objective_model.get_weights())
+
+outer_model = tf.keras.models.Sequential([
+    nom_layer,
+    objective_model
+])
+
+
+def identity_loss(y_true, y_pred):
+    return y_pred
+
+outer_model.compile(optimizer='adam', loss=identity_loss)
+
+initial_guess = np.ones(1)
+dummy_label = np.ones(1)
+
+outer_model.fit(initial_guess, dummy_label, epochs=5, batch_size=1)
+
+print("Weights after optimization")
+print(objective_model.get_weights())
+
 
