637be3b165a272da17502fbbec1584ca3c122b19
Spencer Lee <leespen1@msu.edu>
2024-04-17 03:30:34 -0400

    Added function getting results from all jld2 files in current director

--- AML/process_jld2.py
+++ AML/process_jld2.py
@@ -1,7 +1,25 @@
 from juliacall import Main as jl
 import numpy as np
+import glob
 jl.seval("using JLD2")
 
+"""
+Get data from all jld2 files in dirrector, extract only data corresponding to
+specified problem type and number of frequencies
+"""
+def get_multiple_data(problemtype="results1", N_freq=2):
+    jld2_filenames = glob.glob("./*.jld2")
+    relevant_entries = []
+    for filename in jld2_filenames:
+        relevant_entries += get_data(filename, problemtype, N_freq)
+
+    return relevant_entries
+
+
+"""
+Get data from jld2 corresponding to specified problem type and number of
+frequencies
+"""
 def get_data(data_filename, problemtype="results1", N_freq=2):
     data = jl.load(data_filename)
     relevant_entries = [entry for entry in data[problemtype] if jl.size(entry["om"], 2) == N_freq]
