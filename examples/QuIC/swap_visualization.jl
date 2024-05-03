39d4b78b3e52c4dfd93bb30208f4073bc599be95
Spencer Lee <leespen1@msu.edu>
2024-05-03 15:27:05 -0400

    Changed final time to pi

--- examples/QuIC/swap_visualization.jl
+++ examples/QuIC/swap_visualization.jl
@@ -1,9 +1,10 @@
 using GLMakie
 using QuantumGateDesign
 
-prob = QuantumGateDesign.rotating_frame_qubit(2, 0, tf=10.0, nsteps=1000,
-                                              detuning_frequency=0.0,
-                                              self_kerr_coefficient=0.0)
+#prob = QuantumGateDesign.rotating_frame_qubit(2, 0, tf=10.0, nsteps=1000,
+#                                              detuning_frequency=0.0,
+#                                              self_kerr_coefficient=0.0)
+prob = QuantumGateDesign.construct_rabi_prob(tf=pi)
 
 control = QuantumGateDesign.GRAPEControl(1, prob.tf)
 
