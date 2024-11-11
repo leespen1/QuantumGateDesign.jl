e94ff261f559dc773de75bbcd8029fd994ee96da
Spencer Lee <leespen1@msu.edu>
2024-11-11 00:43:37 -0500

    Adjustments to BasicBSpline

--- src/Controls/BasicBSplineControl.jl
+++ src/Controls/BasicBSplineControl.jl
@@ -7,23 +7,18 @@ struct BasicBSplineControl{degree} <: AbstractControl
     tf::Float64
     N_knots::Int64
     P::BasicBSpline.BSplineSpace{degree, Float64, BasicBSpline.KnotVector{Float64}}
-    P1::BSplineDerivativeSpace{1, BSplineSpace{degree, Float64, BasicBSpline.KnotVector{Float64}}, Float64}
-    P2::BSplineDerivativeSpace{2, BSplineSpace{degree, Float64, BasicBSpline.KnotVector{Float64}}, Float64}
-    P3::BSplineDerivativeSpace{3, BSplineSpace{degree, Float64, BasicBSpline.KnotVector{Float64}}, Float64}
-    P4::BSplineDerivativeSpace{4, BSplineSpace{degree, Float64, BasicBSpline.KnotVector{Float64}}, Float64}
     function BasicBSplineControl(degree::Integer, tf::Real, N_knots::Integer)
         degree = convert(Int64, degree)
         tf = convert(Float64, tf)
         N_knots = convert(Int64, N_knots)
+
         N_coeff = 2*N_knots
+
         # Make the knots on the interval [0,1], then scale the inputs t when evaluating
         knot_vector = BasicBSpline.KnotVector(LinRange(0, 1, N_knots))
         P = BSplineSpace{degree}(knot_vector)
-        P1 = BSplineDerivativeSpace{1}(P)
-        P2 = BSplineDerivativeSpace{2}(P)
-        P3 = BSplineDerivativeSpace{3}(P)
-        P4 = BSplineDerivativeSpace{4}(P)
-        new{degree}(N_coeff, tf, N_knots, P, P1, P2, P3, P4)
+
+        new{degree}(N_coeff, tf, N_knots, P)
     end
 end
 
@@ -64,20 +59,26 @@ end
 
 function eval_p_derivative(
         control::BasicBSplineControl, t::Real, pcof::AbstractVector{<: Real},
-        order::Integer
+        drv::Integer
     )
+    return eval_p_derivative(control, t, pcof, Derivative(drv))
+end
+
+function eval_p_derivative(
+        control::BasicBSplineControl, t::Real, pcof::AbstractVector{<: Real},
+        drv::Derivative{order}
+    ) where order
     # BSpline is scaled on [0,1], rescale t accordingly
     t_scaled = t / control.tf
 
     # Get BSplineSpace and derivatives
-    P_tup = (control.P, control.P1, control.P2, control.P3, control.P4)
-
+    P_drv = BSplineDerivativeSpace{order}(control.P)
 
     # Get the index of the first BSpline Basis function with support at t
     i = BasicBSpline.intervalindex(control.P, t_scaled)
 
     bspline_val = 0.0
-    bspline_basis_vals = BasicBSpline.bsplinebasisall(P_tup[1+order], i, t_scaled)
+    bspline_basis_vals = BasicBSpline.bsplinebasisall(P_drv, i, t_scaled)
     for (n, basis_val) in enumerate(bspline_basis_vals)
         # Multiply basis function value by control point value
         bspline_val += pcof[i-1+n] * basis_val
@@ -118,8 +119,15 @@ a numerical method of order greater than 10)
 """
 function fill_p_vec!(
         vals_vec::AbstractVector{<: Real}, control::BasicBSplineControl, t::Real,
-        pcof::AbstractVector{<: Real}
-    ) 
+        pcof::AbstractVector{<: Real}, max_drv::Derivative{N}
+    ) where N
+
+    drvs = map(QuantumGateDesign.Derivative, ntuple(i -> i-1, N+1))
+    drv_vals = map(drv -> eval_p_derivative(control, t, pcof, drv), drvs)
+    copyto!(vals_vec, 1, drv_vals, 1, min(length(vals_vec), length(drv_vals)))
+
+    return vals_vec
+    #=
     # BSpline is scaled on [0,1], rescale t accordingly
     t_scaled = t / control.tf
 
@@ -142,12 +150,20 @@ function fill_p_vec!(
         vals_vec[k] = bspline_val
     end
     return vals_vec
+    =#
 end
 
 function fill_q_vec!(
         vals_vec::AbstractVector{<: Real}, control::BasicBSplineControl, t::Real,
-        pcof::AbstractVector{<: Real}
-    ) 
+        pcof::AbstractVector{<: Real}, max_drv::Derivative{N}
+    ) where N
+
+    drvs = map(QuantumGateDesign.Derivative, ntuple(i -> i-1, N+1))
+    drv_vals = map(drv -> eval_q_derivative(control, t, pcof, drv), drvs)
+    copyto!(vals_vec, 1, drv_vals, 1, min(length(vals_vec), length(drv_vals)))
+
+    return vals_vec
+    #=
     # BSpline is scaled on [0,1], rescale t accordingly
     t_scaled = t / control.tf
 
@@ -170,4 +186,5 @@ function fill_q_vec!(
         vals_vec[k] = bspline_val
     end
     return vals_vec
+    =#
 end
