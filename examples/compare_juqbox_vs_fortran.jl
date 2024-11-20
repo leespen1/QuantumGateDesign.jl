084170b90721e005735501af2941ab09f845e682
Spencer Lee <leespen1@msu.edu>
2024-11-20 02:31:53 -0500

    Changed some stuff

--- examples/compare_juqbox_vs_fortran.jl
+++ examples/compare_juqbox_vs_fortran.jl
@@ -9,27 +9,27 @@ using Random
 
 tf = 1.0
 D1 = 7 # Number of basis functions in each spline
-#juqbox_control = MySplineControl(tf, D1,)
+juqbox_control = MySplineControl(tf, D1)
 
-degree = 3
+degree = 2
 order = degree + 1
 #N_basis_functions = N_knots + order - 2 #(N_nonrepeating_knots + (order-1) + (order-1) - order)
 #N_knots = D1 + 2 - order
-N_knots = 5
+N_knots = D1-1
 fortran_control = FortranBSplineControl(degree, tf, N_knots)
 
 package_bspline = GeneralBSplineControl(degree, N_knots, tf)
 
-#@assert juqbox_control.N_coeff == fortran_control.N_coeff == package_bspline.N_coeff
-@assert fortran_control.N_coeff == package_bspline.N_coeff
+@assert juqbox_control.N_coeff == fortran_control.N_coeff == package_bspline.N_coeff
+#@assert fortran_control.N_coeff == package_bspline.N_coeff
 N_coeff = fortran_control.N_coeff
 pcof = rand(MersenneTwister(0), N_coeff)
-pcof = ones(N_coeff)
-pcof[end] = pcof[1] = 0
+#pcof = ones(N_coeff)
+#pcof[div(N_coeff,2)] = pcof[1] = 0
 pcof = repeat(pcof, 3)
 
-controls = [fortran_control, package_bspline]
-QuantumGateDesign.plot_controls(controls, pcof, derivative_orders=0:3, control_indices=1:2)
+controls = [juqbox_control, fortran_control, package_bspline]
+QuantumGateDesign.plot_controls(controls, pcof, derivative_orders=0:3, control_indices=1:3)
 
 # Results seem the same, except that the juqbox version differs a little at the
 # endpoints (and the number of knots there might be different)
