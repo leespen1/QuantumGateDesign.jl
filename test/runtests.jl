73156f8b95c9f13fe0884d39a37e517dac64aa8f
Spencer Lee <leespen1@msu.edu>
2024-11-26 14:16:44 -0500

    Removed HermiteControl tests (since we aren't using them anymore)

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,8 @@ using Test: @testset, @test
 @testset "All Tests" begin
 #include("./hardcoded_derivatives.jl")
 # Test that Control Functions are working properly
-include("./ControlFunctionTests/quintic_hermite_interpolation.jl")
 include("./ControlFunctionTests/test_control_derivatives.jl")
 include("./ControlFunctionTests/test_control_gradients.jl")
-include("./ControlFunctionTests/hermite_polynomial.jl")
 # Test that IVPs are converging with correct order
 include("./ConvergenceTests/forward_convergence.jl")
 # Test that gradients are being computed consistently
@@ -15,3 +13,6 @@ include("./GradientTests/compare_gradients.jl")
 # Check that the gate design/optimization problem is behaving properly
 include("./OptimizationTests/optimization_rabi_osc_SWAP.jl")
 end
+## Not using Hermite controls anymore
+#include("./ControlFunctionTests/quintic_hermite_interpolation.jl")
+#include("./ControlFunctionTests/hermite_polynomial.jl")
