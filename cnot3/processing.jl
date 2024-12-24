b3ce18a1850a7b0b1f434aea01aad290f7c1325b
Spencer Lee <leespen1@msu.edu>
2024-12-23 19:27:25 -0500

    Added check for 'watchdog' phases in plotting processing

--- cnot3/processing.jl
+++ cnot3/processing.jl
@@ -27,6 +27,40 @@ function remove_outliers(data, Npoints=10)
     return processed_data
 end
 
+"""
+Function to extract iteration numbers where alpha_pr ends with 'w' (for watchdog)
+"""
+function extract_watchdog_iterations(log_filename::String)
+    iteration_numbers = Int[]
+
+    input_string = read(log_filename, String)
+    lines = split(input_string, "\n")
+
+    # Find the start of the ipopt section of looking for header
+    header = "iter    objective    inf_pr   inf_du lg(mu)  ||d||  lg(rg) alpha_du alpha_pr  ls"
+    start_line = findfirst(line -> line == header, lines)
+
+    for line in lines[start_line:end] # TODO change end to be at the closing statement of IPOPT or something
+        # Only do iteration lines (which start with whitespace, followed by digits)
+        if occursin(r"^\s*\d+", line)
+            columns = split(line)
+
+            if length(columns) == 10
+                alpha_pr = columns[end-1]
+
+                # Only push iteration number if it is not for a watchog phase (alpha_pr doesn't end with 'w')
+                if !endswith(alpha_pr, "w")
+                    # Extract the iteration number from the beginning of the line
+                    iteration = parse(Int, columns[1])
+                    push!(iteration_numbers, iteration)
+                end
+            end
+        end
+    end
+
+    return iteration_numbers
+end
+
 directories = readdir(".")
 # \d+ matches one or more digits
 # Other group captures a scientific notation number, or an integer (tries scientific notation first)
@@ -40,6 +74,7 @@ file_pattern = r"""cnot3_opt(?:_juqbox)?_order=(\d+)
     _date=(.*) # Capture any character 0 or more times
     \.jld2"""x # 'x' tag ignores whitespace and comments
 
+
 pls = [plot(ylims=(-8,0)) for i in 1:12]
 
 # For all directories
@@ -47,8 +82,8 @@ for dir_entry in directories
     # If the directory name matches the regex pattern
     if occursin(dir_pattern, dir_entry)
         regex_match = match(dir_pattern, dir_entry)
-        order = regex_match[1]
-        target_error = regex_match[2]
+        order_str = regex_match[1]
+        target_error_str = regex_match[2]
         #println("Directory match! Order=", order, ". TargetError=", target_error, ". Directory name is", dir_entry)
 
         subdirectories = readdir(dir_entry)
@@ -74,18 +109,32 @@ for dir_entry in directories
                     else
                         opt_history = QuantumGateDesign.read_optimization_history(dir_entry * "/" * subdir_entry)
                         times = opt_history.wall_time ./ 3600 # Time in hours
+
                         infidelities = opt_history.infidelity
                         println("\t# Iterations=", length(infidelities))
                         #infidelities = opt_history.ipopt_obj_value
                     end
 
+                    #TODO Add processing to determing if in watchdog phase
+                    log_pattern = Regex("cnot3_optimization_order=$(order_str)_targeterror=$(target_error_str)_seed=$(seed).*\\.log")
+                    log_files = filter(f -> occursin(log_pattern, f), subdirectories)
+                    display(log_pattern)
+                    display(log_files)
+                    @assert length(log_files) == 1
+                    log_file = first(log_files) # There should 
+
+                    iteration_numbers = extract_watchdog_iterations(dir_entry * "/" * log_file)
+                    iteration_numbers .+= 1
+
+                    
+                    iter_count = opt_history.iter_count
 
                     #infidelities = replace(x -> x < 0 ? NaN : x, infidelities)
                     infidelities = replace(abs, infidelities)
-                    infidelities = remove_outliers(infidelities)
                     log10_infidelities = log10.(infidelities)
                     
-                    plot!(pls[order], times, log10_infidelities, title="Order=$order", linewidth=1)
+                    plot!(pls[order], iter_count, log10_infidelities, title="Order=$order", linewidth=1)
+                    #plot!(pls[order], times[iteration_numbers], log10_infidelities[iteration_numbers], title="Order=$order", linewidth=1)
                     #plot!(pls[order], log10_infidelities, title="Order=$order", linewidth=1)
                 end
             end
