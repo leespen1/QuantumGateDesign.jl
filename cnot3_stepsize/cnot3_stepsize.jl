bb3a8994f05a34321c8024cd7df00f6b2b9c9c8d
Spencer Lee <leespen1@msu.edu>
2025-01-21 03:20:50 +0100

    Changed from JLD2 to exclusively using CSV

--- cnot3_stepsize/cnot3_stepsize.jl
+++ cnot3_stepsize/cnot3_stepsize.jl
@@ -1,19 +1,24 @@
 using QuantumGateDesign, DelimitedFiles, JLD2
-using Juqbox, JLD2, Dates, Printf, Random, LinearAlgebra, Pkg, InteractiveUtils, ArgParse
+using Juqbox, Dates, Printf, Random, LinearAlgebra, Pkg, InteractiveUtils, ArgParse
 using QuantumGateDesign: setup_cnot3, get_D1, get_controls
 
 function collect_data(prob::SchrodingerProb, controls,
         pcof::AbstractVector{<: Real}, order::Integer, max_walltime::Real,
-        filename_base::AbstractString
+        filename_base::AbstractString, N_timestep_saves::Integer
     )
-    max_walltime *= 60*60 # convert walltime from hours to seconds
-
-    initial_time = time()
     prob = copy(prob) # Copy problem, just to make sure there are no mutability issues.
 
-    filename_jld2 = filename_base * ".jld2"
+    initial_time = time()
+    max_walltime *= 60*60 # convert walltime from hours to seconds
+    
+
     filename_csv = filename_base * ".csv"
+    filename_final_states_csv = filename_base * "_finalStates.csv"
+    filename_state_saves_csv = filename_base * "_Nsaves=$(N_timestep_saves).csv"
+
     csv_data::Matrix{Any} = ["nsteps" "stepsize" "R_abs_err_L1" "R_abs_err_L2" "R_rel_err_L1" "R_rel_err_L2" "R_abs_err_Linf" "elapsed_time"]
+    final_states = Matrix{ComplexF64}(undef, 0, prob.N_tot_levels*prob.N_initial_conditions)
+    state_saves = Matrix{ComplexF64}(undef, 0, prob.N_tot_levels*prob.N_initial_conditions*(1+N_timestep_saves))
 
     # Run simulation
     prob.nsteps = 2
@@ -22,15 +27,17 @@ function collect_data(prob::SchrodingerProb, controls,
     history_2h = eval_forward(prob, controls, pcof, order=order)
     t2 = time()
     elapsed_time = t2 - t1
-    # Log data (CSV)
+    # Store/process data
+    final_states = [final_states; reshape(history_2h[:,end,:], 1, :)]
+    state_saves = [state_saves; parse_history_for_csv(history_2h, N_timestep_saves)]
+
     csv_row = transpose([prob.nsteps, stepsize, NaN, NaN, NaN, NaN, NaN, elapsed_time])
     csv_data = [csv_data; csv_row]
+
+    # Log data (CSV)
     writedlm(filename_csv, csv_data, ',')
-    println(csv_row)
-    # Log data (JLD2)
-    jldopen(filename_jld2, "w") do file
-        file[string(prob.nsteps)] = history_2h
-    end
+    writedlm(filename_final_states_csv, final_states, ',')
+    writedlm(filename_state_saves_csv, state_saves, ',')
 
     # Loop until time runs out (with estimator for when we will go overtime on next simulation)
     while (time()-initial_time) < (max_walltime - 2*elapsed_time)
@@ -52,25 +59,62 @@ function collect_data(prob::SchrodingerProb, controls,
         t2 = time()
         elapsed_time = t2 - t1
 
-        # Log data (CSV)
+        # Store/process data
+        final_states = [final_states; reshape(history_h[:,end,:], 1, :)]
+        state_saves = [state_saves; parse_history_for_csv(history_2h, N_timestep_saves)]
+
         R = QuantumGateDesign.RichardsonExtrapolation(history_h[:,1:2:end,:], history_2h, order)
         csv_row = [prob.nsteps stepsize R.abs_err_L1 R.abs_err_L2 R.rel_err_L1 R.rel_err_L2 R.abs_err_Linf elapsed_time]
         csv_data = [csv_data; csv_row]
+
+        # Log data (CSV)
         writedlm(filename_csv, csv_data, ',')
-        println(csv_row)
+        writedlm(filename_final_states_csv, final_states, ',')
+        writedlm(filename_state_saves_csv, state_saves, ',')
+
         println("Size of csv_data:\t", sizeof(csv_data))
         println("Size of history_h:\t", sizeof(history_h))
         println("Size of free memory:\t", Int(Sys.free_memory()))
 
-        # Log data (JLD2)
-        jldopen(filename_jld2, "a+") do file
-            file[string(prob.nsteps)] = history_h
-        end
 
         history_2h = history_h
     end
 
     return readdlm(filename_csv, ',')
+end
+
+"""
+Turn state vector history into a row matrix with the correct number of timesteps
+saved, for putting into a csv file. 
+
+If the number of timesteps to be saved is greater than the number of timesteps
+in the provided history, use NaN in place of the 'missing' timestep saves
+"""
+function parse_history_for_csv(history::AbstractArray{ComplexF64, 3}, N_timestep_saves::Union{Missing, Integer})
+    if ismissing(N_timestep_saves)
+        return copy(reshape(history, 1, :))
+    end
+
+    parsed_history = Array{ComplexF64, 3}(undef, size(history, 1), 1+N_timestep_saves, size(history, 3))
+    parsed_history .= NaN
+
+    nsteps = size(history, 2) - 1
+
+    if  nsteps >= N_timestep_saves
+        if (nsteps % N_timestep_saves != 0)
+            throw(ArgumentError("Number of timesteps to save ($N_timestep_saves) does not divide the number of timesteps ($nsteps)"))
+        end
+        stride = div(nsteps, N_timestep_saves)
+        parsed_history .= history[:,1:stride:end,:]
+    else
+        if (N_timestep_saves % nsteps != 0)
+            throw(ArgumentError("Number of timesteps ($nsteps) does not divide the number of timesteps to save ($N_timestep_saves)"))
+        end
+        stride = div(N_timestep_saves, nsteps)
+        parsed_history[:,1:stride:end,:] .= history
+    end
+
+    return reshape(parsed_history, 1, :)
 end
 
 function parse_commandline()
@@ -89,6 +133,10 @@ function parse_commandline()
             help = "Amount of wall time (in hours) to spend on test."
             arg_type = Float64
             default = 1 # Default
+        "--nsaves", "-s"
+            help = "Number of timesteps to save in CSV files."
+            arg_type = Int64
+            default = 64 # Default
         "order"
             help = "Method order to use"
             required = true
@@ -110,18 +158,21 @@ function main()
     rtol = parsed_args["rtol"]
     D1 = parsed_args["D1"]
     time = parsed_args["time"]
+    N_timestep_saves = parsed_args["nsaves"]
+
     nthreads = Threads.nthreads()
     cnot3ret = QuantumGateDesign.setup_cnot3(seed=seed, rtol=rtol, D1=D1)
     controls = get_controls(order, D1, cnot3ret.juqbox_params.Cfreq, cnot3ret.tf)
 
     N_coeff = QuantumGateDesign.get_number_of_control_parameters(controls)
+
     # Coefficients uniformly distributed between amax and -amax
     pcof = cnot3ret.amax * 2* (0.5 .- rand(MersenneTwister(seed), N_coeff))
 
     mkpath("Data")
     filename = "Data/cnot3StepsizeTest_seed=$(seed)_order=$(order)_rtol=$(rtol)_D1=$(D1)_time=$(time)_nthreads=$(nthreads)"
 
-    collect_data(cnot3ret.qgd_prob, controls, cnot3ret.pcof0, order, time, filename)
+    collect_data(cnot3ret.qgd_prob, controls, cnot3ret.pcof0, order, time, filename, N_timestep_saves)
 end
 
 main()
