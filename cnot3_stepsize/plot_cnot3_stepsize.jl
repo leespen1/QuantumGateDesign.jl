5fab77cc1b195ade54c7fad11543582ebb7febbe
Spencer Lee <leespen1@msu.edu>
2025-03-24 00:49:49 -0700

    Reorgnized plotting and getting stepsize utils, more readable now

--- cnot3_stepsize/plot_cnot3_stepsize.jl
+++ cnot3_stepsize/plot_cnot3_stepsize.jl
@@ -1,211 +1,28 @@
-using CairoMakie, DelimitedFiles, LaTeXStrings, LinearAlgebra, Statistics
-using IterTools
-#using Typst_jll
+using CairoMakie, LaTeXStrings, Statistics
 import Makie
 CairoMakie.set_theme!(CairoMakie.theme_latexfonts())
 
-function get_data(x_header, y_header, out_order, data_directory=missing, juqbox=false)
-    file_pattern= r"""cnot3StepsizeTest
-    _order=(\d+)
-    _degree=(\d+)
-    _seed=(\d+)
-    _atol=(.*)
-    _rtol=(.*)
-    _D1=(\d+)
-    _time=(.*)
-    _nthreads=(\d+)
-    (?:_usejuqbox=(true|false))? # Optionally match , doesn't appear in older files
-    \.csv
-    """x # 'x' tag ignores whitespace and comments
-
-    x_data_entries = Any[]
-    y_data_entries = Any[]
-
-    if ismissing(data_directory)
-        data_directory = dirname(@__FILE__) * "/Data/"
-    end
-
-    files_found = 0
-    files = readdir(data_directory)
-    for file in files
-        if occursin(file_pattern, file)
-            regex_match = match(file_pattern, file)
-            order    = parse(Int,     regex_match[1])
-            degree   = parse(Int,     regex_match[2])
-            seed     = parse(Int,     regex_match[3])
-            atol     = parse(Float64, regex_match[4])
-            rtol     = parse(Float64, regex_match[5])
-            D1       = parse(Int,     regex_match[6])
-            time     = parse(Float64, regex_match[7])
-            nthreads = parse(Int,     regex_match[8])
-            usejuqbox = regex_match[9] == "true" ? true : false
-
-            if ((order == out_order) && (juqbox == usejuqbox))
-                files_found += 1
-                filepath = data_directory * "/" * file
-
-                data, header = readdlm(filepath, ',', header=true)
-                header = vec(header)
-               
-                x_index = findfirst(x -> x == x_header, header)
-                y_index = findfirst(x -> x == y_header, header)
-
-                x_data = data[:, x_index]
-                y_data = data[:, y_index]
-
-                push!(x_data_entries, x_data)
-                push!(y_data_entries, y_data)
-            end
-        end
-    end
-
-    if files_found == 0
-        @warn "No files found matching conditions!"
-    end
-
-    return x_data_entries, y_data_entries
-
-end
-
-function get_x_vec_y_mat(x_data_entries, y_data_entries)
-    if length(x_data_entries) == 0
-        @warn "Length of data entries is zero, returning empty vectors and matrices"
-        return zeros(0), zeros(0,0), zeros(0,0)
-    end
-
-    x_data_lengths = length.(x_data_entries)
-    max_length = maximum(x_data_lengths)
-    min_length = minimum(x_data_lengths)
-    if max_length != min_length 
-        @warn "Not all x_data entries are the same length. Max is $max_length, min is $min_length."
-    end
-
-    # Get the vector from x_data_entries with the longest length
-    x_vec = argmax(length, x_data_entries)
-
-    n_entries = length(x_data_entries)
-    x_mat = fill(NaN, max_length, n_entries)
-    y_mat = fill(NaN, max_length, n_entries)
-    for i in 1:n_entries
-        x_data = x_data_entries[i]
-        y_data = y_data_entries[i]
-        l = length(y_data)
-
-        x_mat[1:l,i] .= x_data
-        y_mat[1:l,i] .= y_data
-    end
-
-    return x_vec, x_mat, y_mat
-end
-
-function get_data_final_states(out_order, data_directory=missing, juqbox=false)
-    file_pattern= r"""cnot3StepsizeTest
-    _order=(\d+)
-    _degree=(\d+)
-    _seed=(\d+)
-    _atol=(.*)
-    _rtol=(.*)
-    _D1=(\d+)
-    _time=(.*)
-    _nthreads=(\d+)
-    (?:_usejuqbox=(true|false))? # Optionally match , doesn't appear in older files
-    _finalStates
-    \.csv
-    """x # 'x' tag ignores whitespace and comments
-
-
-
-    nsteps_vec_entries = Any[]
-    relerr_vec_entries = Any[]
-
-    if ismissing(data_directory)
-        data_directory = dirname(@__FILE__) * "/Data/"
-    end
-
-    files_found = 0
-    files = readdir(data_directory)
-    for file in files
-        if occursin(file_pattern, file)
-            regex_match = match(file_pattern, file)
-            order    = parse(Int,     regex_match[1])
-            usejuqbox = regex_match[9] == "true" ? true : false
-
-            if (order == out_order) && (usejuqbox == juqbox)
-                files_found += 1
-                filepath = data_directory * "/" * file
-
-                # Read as String first, then convert. Otherwise NaN+NaN*im
-                # won't be interpreted correctly
-                final_states = readdlm(filepath, ',', String)
-                final_states = map(x -> x == "NaN + NaN*im" ? NaN + NaN*im : parse(ComplexF64, x),
-                                   final_states)
-
-                true_final_state = final_states[end,:]
-                true_final_state_size = norm(true_final_state)
-                rel_err(x) = norm(x - true_final_state) / true_final_state_size
-
-                n_runs = size(final_states, 1)
-
-                nsteps_vec = [2^i for i in 1:n_runs-1]
-                relerr_vec = [rel_err(final_states[i,:]) for i in 1:n_runs-1]
-               
-                push!(nsteps_vec_entries, nsteps_vec)
-                push!(relerr_vec_entries, relerr_vec)
-            end
-        end
-    end
-
-    if files_found == 0
-        @warn "No files found matching conditions!"
-    end
-
-    return nsteps_vec_entries, relerr_vec_entries
-end
-
-"""
-Given points (x1,y1) and (x2,y2), find the value of x for which the line going
-through the two points goes through y.
-
-Note that I will want to pass the logarithm of the points, so that the linear
-interpolation is valid.
-"""
-function find_x(y, x1, y1, x2, y2)
-    m = (y2-y1) / (x2-x1)
-    x = x1 + (y-y1)/m
-    return x
-end
-
-function find_y(x, x1, y1, x2, y2)
-    m = (y2-y1) / (x2-x1)
-    y = y1 + (x-x1)*m
-    return y
-end
+include("processing_stepsize_data.jl")
     
 
-#data_directory = "Data"
-#data_directory = "48854734"
 #data_directory = "./49076519/49076519/"
 data_directory = "./DataMar18/"
-makie_stddev = Any[]
-makie_spaghetti = Any[]
+orders = [2,4,6,8,10,12, "2 (Stormer-Verlet)"]
+colors = vcat(Makie.wong_colors()[1:6], :darkkhaki)
 
-#ticks_10f(i) = (i < 0) ? L"10^{\text{-}%$(-i)}" : L"10^{%$i}"
+### Set up Makie Figures, Axes
 ticks_10f(i) = L"10^{%$i}"
+log10_ticks = (10.0 .^ (-15:15), ticks_10f.(-15:15))
 
 unlabeled_timestep_ticks = (2 .^ (5:5:20), ["" for i in 5:5:20])
 labeled_timestep_ticks = (2 .^ (5:5:20), [L"2^{%$i}" for i in 5:5:20])
 minor_timestep_ticks = 2 .^ (0:20)
 
-log10_ticks = (10.0 .^ (-15:15), ticks_10f.(-15:15))
-#my_minoryticks = (10.0 .^ (-15:5:15), ticks_10f.(-15:5:15))
-# Getting correct figure, font size: https://docs.makie.org/stable/how-to/match-figure-size-font-sizes-and-dpi
-inch = 96
-#fig = CairoMakie.Figure(size=(3.25inch, 4.5inch), fontsize=12, figure_padding=5)
+inch = 96 # Getting correct figure, font size: https://docs.makie.org/stable/how-to/match-figure-size-font-sizes-and-dpi
 fig = CairoMakie.Figure(size=(6inch, 4inch), fontsize=12, figure_padding=5)
 fig2 = CairoMakie.Figure(size=(4inch, 4inch), fontsize=12, figure_padding=5)
 
-
-ax_error = CairoMakie.Axis(
+ax_nsteps_vs_error = CairoMakie.Axis(
     fig[1,1],
     xscale=CairoMakie.log2,
     yscale=CairoMakie.log10,
@@ -216,10 +33,10 @@ ax_error = CairoMakie.Axis(
     xminorticksvisible = true,
     xminorgridvisible = true,
     yticks=log10_ticks,
-    limits=(nothing, (10.0^(-11.25), 10.0^0.25)),
+    limits=((2^5, 2^19), (10.0^(-10.25), 10.0^0.25)),
 )
 
-ax_timing = CairoMakie.Axis(
+ax_nsteps_vs_time = CairoMakie.Axis(
     fig[1,2],
     xscale=CairoMakie.log2,
     yscale=CairoMakie.log10,
@@ -231,12 +48,14 @@ ax_timing = CairoMakie.Axis(
     xminorgridvisible = true,
     #xticks=(my_xticks, my_xticklabels),
     yticks=log10_ticks,
+    limits=(nothing, (10.0^(-3.25), 10.0^3.25)),
+    #limits=((2^5, 2^19), (10.0^(-3.25), 10.0^3.25)),
     #title="Elapsed Time Plot",
 )
 
-#linkxaxes!(ax_error, ax_timing)
+linkxaxes!(ax_nsteps_vs_time, ax_nsteps_vs_error)
 
-ax_err_vs_timing = CairoMakie.Axis(
+ax_error_vs_time = CairoMakie.Axis(
     fig2[1,1],
     xscale=CairoMakie.log10,
     yscale=CairoMakie.log10,
@@ -251,171 +70,67 @@ ax_err_vs_timing = CairoMakie.Axis(
 )
 
 
-
-
-
-#orders = [2,4,6,8,10,12]
-orders = [2,4,6,8,10,12, "2 (Stormer-Verlet)"]
-#orders = [2,4,6,8,"2 (Stormer-Verlet)"]
-colors = vcat(Makie.wong_colors()[1:6], :darkkhaki)
-
-# TODO: merge the two for-loops into one, but this all into a function so I don't need to write `local` so many times
 for (k, order) in enumerate(orders)
-
-    ##=== Nsteps Vs Relative Error Plot ===##
-    
-    println("\nNsteps Vs Relative Eror Plot")
     if order == "2 (Stormer-Verlet)"
-        local nsteps_vec_entries, relerr_vec_entries = get_data_final_states(2, data_directory, true)
+        label = "Order 2 (Stormer-Verlet)"
+        juqbox = true
+        order = 2
     else
-        local nsteps_vec_entries, relerr_vec_entries = get_data_final_states(order, data_directory)
+        label = "Order $order"
+        juqbox = false
     end
-    @show maximum(length, nsteps_vec_entries)
+    ### Collect data
+    target_labels = ("nsteps", "elapsed_time", "R_rel_err_L2")
+    target_symbols = Symbol.(target_labels)
+    entries = NamedTuple{target_symbols}(
+        get_data(target_labels, order, data_directory=data_directory, juqbox=juqbox)
+    )
 
-    local full_nsteps_vec, nsteps_mat, relerr_mat = get_x_vec_y_mat(nsteps_vec_entries, relerr_vec_entries)
+    ### Process data, grabbing data columns from multiple files (possibly with
+    # different lengths), combine them into a matrix
+    full_nsteps_vec = combined_x_vec(entries.nsteps)
+    elapsedtime_ymat = get_y_mat(entries.nsteps, entries.elapsed_time, full_nsteps_vec)
+    elapsedtime_meanvec = mean(elapsedtime_ymat, dims=2) |> vec
+    # Handle relative error from final states, which is calculated from a
+    # separate file, and must be handeled differently
+    relerr_nsteps_entries, relerr_err_entries = get_nsteps_errors_final_states(
+        order, data_directory=data_directory, juqbox=juqbox
+    )
+    relerr_ymat = get_y_mat(relerr_nsteps_entries, relerr_err_entries, full_nsteps_vec)
+    relerr_meanvec = mean(relerr_ymat, dims=2) |> vec
+    
 
-    local relerr_mean =  mean(relerr_mat, dims=2) |> vec
-    local relerr_stddev = std(relerr_mat, dims=2) |> vec
-
-    @show length(full_nsteps_vec) 
-    @show full_nsteps_vec
-
-    local lines_obj = lines!(ax_error, full_nsteps_vec[5:end], relerr_mean[5:end]; color=colors[k], label="Order $order")
+    ##### Make Plots
+    ### Number of Timesteps vs Elapsed Time
+    lines_obj = lines!(
+        ax_nsteps_vs_time, full_nsteps_vec[5:end], elapsedtime_meanvec[5:end];
+        color=colors[k], label=label
+    )
     CairoMakie.translate!(lines_obj, 0, 0, -k) # Put the lines in the right z-order
 
-    ##=== Nsteps Vs Wall Time Plot ===##
-    #
-    println("\nNsteps Vs Wall Time Plot")
-    
-    if order == "2 (Stormer-Verlet)"
-        local nsteps_vec_entries, elapsedtime_vec_entries = get_data("nsteps", "elapsed_time", 2, data_directory, true)
-    else
-        local nsteps_vec_entries, elapsedtime_vec_entries = get_data("nsteps", "elapsed_time", order, data_directory)
-    end
-    @show maximum(length, nsteps_vec_entries)
+    ### Number of Timesteps vs Relative Error in Final State
+    lines_obj = lines!(
+        ax_nsteps_vs_error, full_nsteps_vec[5:end], relerr_meanvec[5:end];
+        color=colors[k], label=label
+    )
+    CairoMakie.translate!(lines_obj, 0, 0, -k) # Put the lines in the right z-order
 
-    local full_nsteps_vec, nsteps_mat, elapsedtime_mat = get_x_vec_y_mat(nsteps_vec_entries, elapsedtime_vec_entries)
-    local elapsedtime_mean =  mean(elapsedtime_mat, dims=2) |> vec
-    local elapsedtime_stddev = std(elapsedtime_mat, dims=2) |> vec
-
-    local lines_obj = lines!(ax_timing, full_nsteps_vec[5:end], elapsedtime_mean[5:end]; color=colors[k])
-    CairoMakie.translate!(lines_obj, 0, 0, -k) # Draw the high-order methods in the back
-
-    ##=== Relative Error Vs Wall Time Plot ===##
-
-    @show length(full_nsteps_vec) 
-    @show full_nsteps_vec
-    @show length(relerr_mean)
-    @show length(elapsedtime_mean)
-    local scatter_obj = lines!(ax_err_vs_timing, relerr_mean[5:end], elapsedtime_mean[6:end]; color=colors[k], label="Order $order")
-    local dummy_relerr_mean = [10.0 ^(-i) for i in 5:10]
-    local dummy_elapsedtime_mean = 0.5 .* (dummy_relerr_mean .^ -0.47)
-    local scatter_obj = lines!(ax_err_vs_timing, dummy_relerr_mean, dummy_elapsedtime_mean; color=colors[k], label="Order $order", linestyle=:dot)
+    ### Relative Error vs Elapsed Time
+    scatter_obj = lines!(
+        ax_error_vs_time, relerr_meanvec[5:end], elapsedtime_meanvec[5:end];
+        color=colors[k], label=label
+    )
+    # Add a hard-coded line to approximately extrapolate 2nd order Hemite and Stormer-Verlet
+    dummy_relerr_mean = [10.0 ^(-i) for i in 5:10]
+    dummy_elapsedtime_mean = 0.25 .* (dummy_relerr_mean .^ -0.47)
+    scatter_obj = lines!(
+        ax_error_vs_time, dummy_relerr_mean, dummy_elapsedtime_mean;
+        color=colors[k], label=label, linestyle=:dot
+    )
     CairoMakie.translate!(scatter_obj, 0, 0, -k) # Put the lines in the right z-order
-
-    #band!(ax_error, full_nsteps_vec, relerr_mean - relerr_stddev, relerr_mean + relerr_stddev; color=colors[k], 0.5))
-
-    #= 
-    #Spaghetti Style
-    for (nsteps_vec, relerr_vec) in zip(nsteps_vec_entries, relerr_vec_entries)
-        CairoMakie.lines!(ax_spaghetti, nsteps_vec[5:end], relerr_vec[5:end]; color=colors[k], 0.5))
-    end
-    =#
 end
-
-
-##=== Getting the target error vs nsteps relationship ===##
-target_errors = collect(-1:-1:-7)
-target_nsteps_mat = fill(NaN, length(target_errors), length(orders))
-target_time_mat = fill(NaN, length(target_errors), length(orders))
-
-for (k, order) in enumerate(orders)
-    if order == "2 (Stormer-Verlet)"
-        local nsteps_vec_entries, relerr_vec_entries = get_data_final_states(2, data_directory, true)
-    else
-        local nsteps_vec_entries, relerr_vec_entries = get_data_final_states(order, data_directory)
-    end
-
-    local full_nsteps_vec, nsteps_mat, relerr_mat = get_x_vec_y_mat(nsteps_vec_entries, relerr_vec_entries)
-
-    if order == "2 (Stormer-Verlet)"
-        @show full_nsteps_vec
-        @show nsteps_mat[:,1]
-        @show relerr_mat[:,1]
-    end
-
-    if order == "2 (Stormer-Verlet)"
-        local nsteps_vec_entries2, elapsedtime_vec_entries = get_data("nsteps", "elapsed_time", 2, data_directory, true)
-    else
-        local nsteps_vec_entries2, elapsedtime_vec_entries = get_data("nsteps", "elapsed_time", order, data_directory)
-    end
-    local full_nsteps_vec2, nsteps_mat, elapsedtime_mat = get_x_vec_y_mat(nsteps_vec_entries2, elapsedtime_vec_entries)
-
-    local elapsedtime_mean = mean(elapsedtime_mat, dims=2) |> vec
-    local relerr_mean =  mean(relerr_mat, dims=2) |> vec
-
-    local full_nsteps_vec = full_nsteps_vec[5:end]
-    local relerr_mean = relerr_mean[5:end]
-    # The richardson table has one more entrie than the data from the final states
-    local elapsedtime_mean = elapsedtime_mean[5:end-1] 
-
-    local log2_nsteps_vec = log2.(full_nsteps_vec)
-    local log10_relerr_vec = log10.(relerr_mean)
-    local log10_elapsedtime_vec = log10.(elapsedtime_mean)
-
-    local zipped_vecs = zip(log2_nsteps_vec, log10_relerr_vec, log10_elapsedtime_vec)
     
-    # This will handle all the target errors that can be done by 
-    for (i, target_error) in enumerate(target_errors)
-        for ((nsteps1, relerr1, time1), (nsteps2, relerr2, time2)) in partition(zipped_vecs, 2, 1)
-            if (relerr1 > target_error > relerr2)
-                target_nsteps = find_x(target_error, nsteps1, relerr1, nsteps2, relerr2)
-                target_time = find_y(target_nsteps, nsteps1, time1, nsteps2, time2)
-                target_nsteps_mat[i,k] = target_nsteps
-                target_time_mat[i,k] = target_time
-            end
-        end
-    end
-
-    # Use Linear Least Squares to estimate remaining entries (which happen for 2nd order method)
-    local asymptotic_nsteps_vec = log2_nsteps_vec[end-6:end] 
-    local asymptotic_relerr_vec = log10_relerr_vec[end-6:end] 
-    local asymptotic_elapsedtime_vec = log10_elapsedtime_vec[end-6:end]
-
-    local A = [ones(7) asymptotic_nsteps_vec]
-
-    # y = mx + b
-    local y_intercept_relerr, slope_relerr = A \ asymptotic_relerr_vec 
-    local y_intercept_time, slope_time = A \ asymptotic_elapsedtime_vec 
-
-    for (i, target_error) in enumerate(target_errors)
-        if (isnan(target_nsteps_mat[i,k])) 
-            target_nsteps = (target_error - y_intercept_relerr) / slope_relerr
-            target_time = y_intercept_time + slope_time*target_nsteps
-
-            target_nsteps_mat[i,k] = target_nsteps
-            target_time_mat[i,k] = target_time
-        end
-    end
-
-end
-
-target_nsteps_mat_nonlog = ceil.(Int64, 2 .^ target_nsteps_mat)
-target_time_mat_nonlog = 10 .^ target_time_mat
-target_time_ratios = copy(target_time_mat_nonlog)
-for i in 1:size(target_time_ratios, 2)
-    target_time_ratios[:,i] ./= target_time_mat_nonlog[:,1]
-end
-target_time_ratios = 1 ./ target_time_ratios
-
-
-Legend(fig[2,:], ax_error, orientation = :horizontal, tellwidth = false, nbanks=2, framevisible=false)
+Legend(fig[2,:], ax_nsteps_vs_error, orientation = :horizontal, tellwidth = false, nbanks=2, framevisible=false)
 rowgap!(fig.layout, 1, 0)
-#rowsize!(fig.layout, 2, Relative(1/3))
 
-fig
-
-
-# Idea, scatter plot with x=desired error, y=expected elapsed time, and put
-# number of timesteps required as an in-graph number annotation
-# Actually, I think a dogged bar plot would be better
+fig2
---
+++ cnot3_stepsize/processing_stepsize_data.jl
@@ -0,0 +1,205 @@
+using DelimitedFiles, LinearAlgebra
+
+function get_data(target_labels, out_order::Integer; data_directory=missing,
+        juqbox::Bool=false)
+
+    file_pattern= r"""cnot3StepsizeTest
+    _order=(\d+)
+    _degree=(\d+)
+    _seed=(\d+)
+    _atol=(.*)
+    _rtol=(.*)
+    _D1=(\d+)
+    _time=(.*)
+    _nthreads=(\d+)
+    (?:_usejuqbox=(true|false))? # Optionally match , doesn't appear in older files
+    (?:_gradient=(true|false))? # Optionally match , doesn't appear in older files
+    \.csv
+    """x # 'x' tag ignores whitespace and comments
+
+    data_entries_collection = ntuple(i -> Vector{Float64}[], length(target_labels))
+
+    if ismissing(data_directory)
+        data_directory = dirname(@__FILE__) * "/Data/"
+    end
+
+    files_found = 0
+    files = readdir(data_directory)
+    for file in files
+        if occursin(file_pattern, file)
+            regex_match = match(file_pattern, file)
+            order    = parse(Int,     regex_match[1])
+            usejuqbox = regex_match[9] == "true" ? true : false
+
+            if ((order == out_order) && (juqbox == usejuqbox))
+                files_found += 1
+                filepath = data_directory * "/" * file
+
+                dlm_data, dlm_header = readdlm(filepath, ',', Float64, header=true)
+                dlm_header = vec(dlm_header)
+
+                for (data_entries, label) in zip(data_entries_collection, target_labels)
+                    index = findfirst(x -> x == label, dlm_header)
+                    target_data = dlm_data[:,index]
+                    push!(data_entries, target_data)
+                end
+            end
+        end
+    end
+
+    if files_found == 0
+        @warn "No files found matching conditions!"
+    end
+
+    return data_entries_collection
+end
+
+function combined_x_vec(x_data_entries::Vector{<: Vector})
+    combined_x_vec = sort(unique(vcat(x_data_entries...)))
+    return combined_x_vec
+end
+
+
+"""
+`x_data_entries` should be a vector of vectors, whose elements should be
+ordered.
+`y_data_entries` should be a vector of vectors, whose lengths match those of
+`x_data_entries`.
+
+Returns a vector and a matrix. The vector is the "combined" version of all the
+entries in `x_data_entries`, and the matrix holds the y values corresponding to
+the the x values in the vector. Each column holds the data from one of entries
+in `y_data_entries`. If that entry does not have a y value for the
+corresponding x value, then `NaN` is used.
+
+If `full_x_vec` is provided, use that to get the desired x values, instead of
+building it from x_data_entries, and don't return an x_vec, only the y_mat
+"""
+function get_y_mat(x_data_entries::Vector{<: Vector},
+                   y_data_entries::Vector{<: Vector},
+                   full_x_vec::Vector{<: Real})
+
+    @assert length(x_data_entries) == length(y_data_entries)
+    for (x_entry, y_entry) in zip(x_data_entries, y_data_entries)
+        @assert length(x_entry) == length(y_entry)
+    end
+
+    max_length = maximum(length, x_data_entries)
+    min_length = minimum(length, x_data_entries)
+    if !allequal(x_data_entries) 
+        @warn "Not all x_data entries are the same."
+    end
+
+    n_xs = length(full_x_vec)
+    n_entries = length(x_data_entries)
+
+    y_mat = fill(NaN, n_xs, n_entries)
+
+    for k in 1:n_entries
+        x_vec = x_data_entries[k]
+        y_vec = y_data_entries[k]
+        for (j, x) in enumerate(full_x_vec)
+            let x = x
+                l = findfirst(a -> a == x, x_vec)
+                y = isnothing(l) ? NaN : y_vec[l]
+                y_mat[j,k] = y
+            end
+        end
+    end
+    return y_mat
+end
+
+function get_x_vec_y_mat(x_data_entries::Vector{<: Vector},
+                         y_data_entries::Vector{<: Vector})
+    full_x_vec = combined_x_vec(x_data_entries)
+    y_mat = get_y_mat(x_data_entries, y_data_entries, full_x_vec)
+    return full_x_vec, y_mat
+end
+
+"""
+Using the final state with the highest number of timesteps as the "true"
+solution, return vector of nsteps vectors and a vector of relerr vectors, with
+an inner vector for each file.
+"""
+function get_nsteps_errors_final_states(out_order; data_directory=missing, juqbox=false)
+    file_pattern = r"""cnot3StepsizeTest
+    _order=(\d+)
+    _degree=(\d+)
+    _seed=(\d+)
+    _atol=(.*)
+    _rtol=(.*)
+    _D1=(\d+)
+    _time=(.*)
+    _nthreads=(\d+)
+    (?:_usejuqbox=(true|false))? # Optionally match , doesn't appear in older files
+    (?:_gradient=(true|false))? # Optionally match , doesn't appear in older files
+    _finalStates
+    \.csv
+    """x # 'x' tag ignores whitespace and comments
+
+
+    nsteps_vec_entries = Vector{Float64}[]
+    relerr_vec_entries = Vector{Float64}[]
+
+    if ismissing(data_directory)
+        data_directory = dirname(@__FILE__) * "/Data/"
+    end
+
+    files_found = 0
+    files = readdir(data_directory)
+    for file in files
+        if occursin(file_pattern, file)
+            regex_match = match(file_pattern, file)
+            order    = parse(Int,     regex_match[1])
+            usejuqbox = regex_match[9] == "true" ? true : false
+
+            if (order == out_order) && (usejuqbox == juqbox)
+                files_found += 1
+                filepath = data_directory * "/" * file
+
+                # Read as String first, then convert. Otherwise NaN+NaN*im
+                # won't be interpreted correctly
+                final_states = readdlm(filepath, ',', String)
+                final_states = map(x -> x == "NaN + NaN*im" ? NaN + NaN*im : parse(ComplexF64, x),
+                                   final_states) 
+
+                true_final_state = final_states[end,:]
+                true_final_state_size = norm(true_final_state)
+                rel_err(x) = norm(x - true_final_state) / true_final_state_size
+
+                n_runs = size(final_states, 1)
+
+                nsteps_vec = [2^i for i in 1:n_runs]
+                relerr_vec = vcat([rel_err(final_states[i,:]) for i in 1:n_runs-1], NaN) # "True" final state has no comparison point, so use NaN as error
+               
+                push!(nsteps_vec_entries, nsteps_vec)
+                push!(relerr_vec_entries, relerr_vec)
+            end
+        end
+    end
+
+    if files_found == 0
+        @warn "No files found matching conditions!"
+    end
+
+    return nsteps_vec_entries, relerr_vec_entries
+end
+
+"""
+Given points (x1,y1) and (x2,y2), find the value of x for which the line going
+through the two points goes through y.
+
+Note that I will want to pass the logarithm of the points, so that the linear
+interpolation is valid.
+"""
+function find_x(y, x1, y1, x2, y2)
+    m = (y2-y1) / (x2-x1)
+    x = x1 + (y-y1)/m
+    return x
+end
+
+function find_y(x, x1, y1, x2, y2)
+    m = (y2-y1) / (x2-x1)
+    y = y1 + (x-x1)*m
+    return y
+end
