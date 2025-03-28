dbeca3b418231e89b5586ef9c5d4f9ffcd20a7cb
Spencer Lee <leespen1@msu.edu>
2025-03-28 15:11:48 -0700

    Put two figures into one, adjusted data collection, now uses time for whole gradient

--- cnot3_stepsize/plot_cnot3_stepsize.jl
+++ cnot3_stepsize/plot_cnot3_stepsize.jl
@@ -5,10 +5,10 @@ CairoMakie.set_theme!(CairoMakie.theme_latexfonts())
 include("processing_stepsize_data.jl")
     
 
-#data_directory = "./49076519/49076519/"
-data_directory = "./52547452/"
-#orders = [2,4,6,8,10,12, "2 (Stormer-Verlet)"]
-orders = [2,4,6,8,10,12]
+data_directory = "./DataMar28/"
+orders = [2,4,6,8,10,12, "2 (Stormer-Verlet)"]
+#orders = [2,4,6,8,10,12]
+gradient = true
 colors = vcat(Makie.wong_colors()[1:6], :darkkhaki)
 
 ### Set up Makie Figures, Axes
@@ -20,16 +20,19 @@ labeled_timestep_ticks = (2 .^ (5:5:20), [L"2^{%$i}" for i in 5:5:20])
 minor_timestep_ticks = 2 .^ (0:20)
 
 inch = 96 # Getting correct figure, font size: https://docs.makie.org/stable/how-to/match-figure-size-font-sizes-and-dpi
-fig = CairoMakie.Figure(size=(6inch, 4inch), fontsize=12, figure_padding=5)
-fig2 = CairoMakie.Figure(size=(4inch, 4inch), fontsize=12, figure_padding=5)
+fig = CairoMakie.Figure(size=(7inch, 5inch), fontsize=12, figure_padding=(0,0.075inch,0,0.05inch))
+
+nsteps_vs_error_grid = fig[1,1]
+nsteps_vs_time_grid = fig[2,1]
+error_vs_time_grid = fig[1:2,2]
 
 ax_nsteps_vs_error = CairoMakie.Axis(
-    fig[1,1],
+    nsteps_vs_error_grid,
     xscale=CairoMakie.log2,
     yscale=CairoMakie.log10,
-    xlabel="Number of Timesteps",
+    #xlabel="Number of Timesteps",
     ylabel="Mean Relative Error",
-    xticks=labeled_timestep_ticks,
+    xticks=unlabeled_timestep_ticks,
     xminorticks = minor_timestep_ticks,
     xminorticksvisible = true,
     xminorgridvisible = true,
@@ -38,36 +41,40 @@ ax_nsteps_vs_error = CairoMakie.Axis(
 )
 
 ax_nsteps_vs_time = CairoMakie.Axis(
-    fig[1,2],
+    nsteps_vs_time_grid,
     xscale=CairoMakie.log2,
     yscale=CairoMakie.log10,
     xlabel="Number of Timesteps",
-    ylabel="Mean Elapsed Wall Time (s)",
+    ylabel="Mean Time To Compute Gradient (s)",
     xticks=labeled_timestep_ticks,
     xminorticks = minor_timestep_ticks,
     xminorticksvisible = true,
     xminorgridvisible = true,
     #xticks=(my_xticks, my_xticklabels),
     yticks=log10_ticks,
-    limits=(nothing, (10.0^(-3.25), 10.0^3.25)),
+    limits=(nothing, (10.0^(-2.25), 10.0^3.25)),
     #limits=((2^5, 2^19), (10.0^(-3.25), 10.0^3.25)),
     #title="Elapsed Time Plot",
 )
 
+# Share the x-axis
 linkxaxes!(ax_nsteps_vs_time, ax_nsteps_vs_error)
 
 ax_error_vs_time = CairoMakie.Axis(
-    fig2[1,1],
+    error_vs_time_grid,
     xscale=CairoMakie.log10,
     yscale=CairoMakie.log10,
     xlabel="Mean Relative Error",
-    ylabel="Mean Elapsed Wall Time (s)",
+    ylabel="Mean Time To Compute Gradient (s)",
     #xminorticksvisible = true,
     #xminorgridvisible = true,
     xticks=log10_ticks,
     yticks=log10_ticks,
+    yminorticks=IntervalsBetween(10),
+    yminorticksvisible=true,
+    yminorgridvisible=true,
     #title="Elapsed Time Plot",
-    limits=((10.0^(-10.25), 10.0^(-0.75)), (1e-1,1e5)),
+    limits=((10.0^(-10.25), 10.0^(-0.75)), (10.0^(-0.25),10.0^(5.25))),
 )
 
 
@@ -81,7 +88,7 @@ for (k, order) in enumerate(orders)
         juqbox = false
     end
     ### Collect data
-    target_labels = ("nsteps", "elapsed_time", "R_rel_err_L2")
+    target_labels = ("nsteps", "elapsed_time", "forward_time", "adjoint_time", "grad_accum_time",  "R_rel_err_L2")
     target_symbols = Symbol.(target_labels)
     entries = NamedTuple{target_symbols}(
         get_data(target_labels, order, data_directory=data_directory, juqbox=juqbox, gradient=gradient)
@@ -95,11 +102,10 @@ for (k, order) in enumerate(orders)
     # Handle relative error from final states, which is calculated from a
     # separate file, and must be handeled differently
     relerr_nsteps_entries, relerr_err_entries = get_nsteps_errors_final_states(
-        order, data_directory=data_directory, juqbox=juqbox
+        order, data_directory=data_directory, juqbox=juqbox, gradient=gradient
     )
     relerr_ymat = get_y_mat(relerr_nsteps_entries, relerr_err_entries, full_nsteps_vec)
     relerr_meanvec = mean(relerr_ymat, dims=2) |> vec
-    
 
     ##### Make Plots
     ### Number of Timesteps vs Elapsed Time
@@ -121,17 +127,21 @@ for (k, order) in enumerate(orders)
         ax_error_vs_time, relerr_meanvec[5:end], elapsedtime_meanvec[5:end];
         color=colors[k], label=label
     )
-    # Add a hard-coded line to approximately extrapolate 2nd order Hemite and Stormer-Verlet
-    dummy_relerr_mean = [10.0 ^(-i) for i in 5:10]
-    dummy_elapsedtime_mean = 0.25 .* (dummy_relerr_mean .^ -0.47)
-    scatter_obj = lines!(
-        ax_error_vs_time, dummy_relerr_mean, dummy_elapsedtime_mean;
-        color=colors[k], label=label, linestyle=:dot
-    )
-    CairoMakie.translate!(scatter_obj, 0, 0, -k) # Put the lines in the right z-order
 end
-    
-Legend(fig[2,:], ax_nsteps_vs_error, orientation = :horizontal, tellwidth = false, nbanks=2, framevisible=false)
-rowgap!(fig.layout, 1, 0)
 
-fig2
+# Add a hard-coded line to approximately extrapolate 2nd order Hemite and Stormer-Verlet
+dummy_relerr_mean = [10.0 ^(-i) for i in 4.25:11]
+dummy_elapsedtime_mean = 1.1 .* (dummy_relerr_mean .^ -0.47)
+lines!(
+    ax_error_vs_time, dummy_relerr_mean, dummy_elapsedtime_mean;
+    color=:black, linestyle=:dot, label="Order 2 (Approximate Extrapolation)"
+)
+    
+# Add legend to very end
+Legend(fig[end+1,:], ax_error_vs_time, orientation = :horizontal, tellwidth = false, nbanks=2, framevisible=false)
+#rowsize!(fig.layout, 1, Relative(0.6))
+rowgap!(fig.layout, 1, 0.1inch) # Minimize space between legend and plot area.
+rowgap!(fig.layout, 2, 0inch) # Minimize space between legend and plot area.
+colgap!(fig.layout, 1, 0.1inch) # Minimize space between legend and plot area.
+
+fig
