60feb4e78394c3069474d0bab9912102293ff85f
Spencer Lee <leespen1@msu.edu>
2025-05-04 03:12:27 -0400

    Added chunking

--- cnot3_accuracy_verification/cnot3_accuracy_verification.jl
+++ cnot3_accuracy_verification/cnot3_accuracy_verification.jl
@@ -12,7 +12,7 @@ else
 end
 
 @everywhere begin
-    #println("[After addprocs] Hello from $(myid()):$(gethostname())\nCurrent project environemtn $(Base.active_project())\nCurrent Directory: $(pwd())")
+    println("[After addprocs] Hello from $(myid()):$(gethostname())\nCurrent project environemtn $(Base.active_project())\nCurrent Directory: $(pwd())")
     using QuantumGateDesign, Dates
     using QuantumGateDesign: real_to_complex, get_number_of_control_parameters,
                              DiscreteAdjointTimes, discrete_adjoint!, total_time,
@@ -37,82 +37,83 @@ end
 
 function main()
     println("Starting Main")
-    dir = "/mnt/home/leespen1/Research/QuantumGateDesign.jl/cnot3_optimization/53220812"
-    for filename in readdir(dir, join=true)
-        println("Doing file $filename")
-        #filename = "/home/spencer/Research/QuantumGateDesign.jl/cnot3_optimization/51458831/targetError=1e-1_cnot3OptimizationTest_order=2_degree=14_seed=0_nsteps=7433_atol=1.0e-15_rtol=1.0e-15_D1=16_time=6.0_maxiter=10000_nthreads=4_costType=Infidelity_gateDuration=550.0_nCavityLevels=10_pcof.csv"
-        #filename = "/home/spencer/Research/QuantumGateDesign.jl/cnot3_optimization/51458831/targetError=1e-3_cnot3OptimizationTest_order=4_degree=14_seed=0_nsteps=3535_atol=1.0e-15_rtol=1.0e-15_D1=16_time=6.0_maxiter=10000_nthreads=4_costType=Infidelity_gateDuration=550.0_nCavityLevels=10_pcof.csv"
-        if !contains(filename, "_pcof.csv")
-            continue 
-        end
+    filepath = ENV["CNOT3FILEPATH"]
+    #filepath = "/mnt/ffs24/home/leespen1/Research/QuantumGateDesign.jl/cnot3_optimization/53220812/targetError=1e-1_cnot3OptimizationTest_order=4_degree=14_seed=0_nsteps=825_atol=1.0e-10_rtol=1.0e-10_D1=16_time=6.0_maxiter=10000_nthreads=4_costType=GeneralizedInfidelity_gateDuration=550.0_nCavityLevels=10.csv"
+    #filepath = "/mnt/ffs24/home/leespen1/Research/QuantumGateDesign.jl/cnot3_optimization/53220812/targetError=1e-3_cnot3OptimizationTest_order=4_degree=14_seed=0_nsteps=3535_atol=1.0e-10_rtol=1.0e-10_D1=16_time=6.0_maxiter=10000_nthreads=4_costType=GeneralizedInfidelity_gateDuration=550.0_nCavityLevels=10.csv"
+    filename = basename(filepath)
 
-        parsed_args = parse_filename_params(filename) 
-        coarse_order = parsed_args["order"]
-        degree = parsed_args["degree"]
-        #coarse_atol = parsed_args["atol"]
-        #coarse_rtol = parsed_args["rtol"]
-        coarse_nsteps = parsed_args["nsteps"]
-        D1 = parsed_args["D1"]
-        Tmax = parsed_args["gateDuration"]
-        N_osc_levels = parsed_args["nCavityLevels"]
-        target_error = parsed_args["targetError"]
+    parsed_args = parse_filename_params(filename) 
+    coarse_order = parsed_args["order"]
+    degree = parsed_args["degree"]
+    #coarse_atol = parsed_args["atol"]
+    #coarse_rtol = parsed_args["rtol"]
+    coarse_nsteps = parsed_args["nsteps"]
+    D1 = parsed_args["D1"]
+    Tmax = parsed_args["gateDuration"]
+    N_osc_levels = parsed_args["nCavityLevels"]
+    target_error = parsed_args["targetError"]
 
-        # Only process low-accuracy files
-        if (target_error != 1e-1 && target_error != 1e-3 ) || (coarse_order != 4)
-            println("Skipping file $filename")
-            continue
-        end
+    coarse_N_deriv = div(coarse_order, 2)
 
-        coarse_N_deriv = div(coarse_order, 2)
+    # Used when aiming for accuracy
+    atol = 1e-15
+    rtol = 1e-15
+    #fine_atol = 1e-15
+    #fine_rtol = 1e-15
+    fine_order = 6
+    fine_nsteps = 5415
+    fine_N_deriv = div(fine_order, 2)
 
-        # Used when aiming for accuracy
-        atol = 1e-15
-        rtol = 1e-15
-        #fine_atol = 1e-15
-        #fine_rtol = 1e-15
-        fine_order = 6
-        fine_nsteps = 5415
-        fine_N_deriv = div(fine_order, 2)
+    cnot3ret = QuantumGateDesign.setup_cnot3(
+        seed=0, # Doesn't matter
+        atol=atol,
+        rtol=rtol,
+        D1=D1,
+        N_osc_levels=N_osc_levels,
+        Tmax=Tmax
+    )
+    cnot3ret2 = QuantumGateDesign.setup_cnot3(
+        seed=0, # Doesn't matter
+        atol=atol,
+        rtol=rtol,
+        D1=D1,
+        N_osc_levels=N_osc_levels,
+        Tmax=Tmax
+    )
+    coarse_prob = cnot3ret.qgd_prob
+    coarse_prob.nsteps = coarse_nsteps
+    fine_prob = cnot3ret2.qgd_prob
+    fine_prob.nsteps = fine_nsteps
+    target = cnot3ret.target
 
-        cnot3ret = QuantumGateDesign.setup_cnot3(
-            seed=0, # Doesn't matter
-            atol=atol,
-            rtol=rtol,
-            D1=D1,
-            N_osc_levels=N_osc_levels,
-            Tmax=Tmax
-        )
-        cnot3ret2 = QuantumGateDesign.setup_cnot3(
-            seed=0, # Doesn't matter
-            atol=atol,
-            rtol=rtol,
-            D1=D1,
-            N_osc_levels=N_osc_levels,
-            Tmax=Tmax
-        )
-        coarse_prob = cnot3ret.qgd_prob
-        coarse_prob.nsteps = coarse_nsteps
-        fine_prob = cnot3ret2.qgd_prob
-        fine_prob.nsteps = fine_nsteps
-        target = cnot3ret.target
+    controls = get_controls(degree, D1, cnot3ret.juqbox_params.Cfreq, cnot3ret.tf)
 
-        controls = get_controls(degree, D1, cnot3ret.juqbox_params.Cfreq, cnot3ret.tf)
+    N_coeff = QuantumGateDesign.get_number_of_control_parameters(controls)
 
-        N_coeff = QuantumGateDesign.get_number_of_control_parameters(controls)
+    header = hcat(
+        "ipopt_iter",
+        "UT_err", "grad_pcof_err", "infidelity_err", "gen_infidelity_err",
+        "coarse_infidelity", "coarse_gen_infidelity", "coarse_norm_grad", "coarse_norm_UT",
+        "fine_infidelity", "fine_gen_infidelity", "fine_norm_grad", "fine_norm_UT",
+    )
 
-        header = hcat(
-            "ipopt_iter",
-            "UT_err", "grad_pcof_err", "infidelity_err", "gen_infidelity_err",
-            "coarse_infidelity", "coarse_gen_infidelity", "coarse_norm_grad", "coarse_norm_UT",
-            "fine_infidelity", "fine_gen_infidelity", "fine_norm_grad", "fine_norm_UT",
-        )
+    pcofs = readdlm(filepath, ',', Float64)
+    #pcofs = pcofs[1:500, :] # Don't do the whole iteration history, too much work
+    pcof_rows = copy.(eachrow(pcofs)) # Copy to convert to plain arrays, not views
+    chunk_size = 10*nworkers()
+    pcof_rows_chunked = copy.(collect.(Iterators.partition(pcof_rows, chunk_size)))
 
-        pcofs = readdlm(filename, ',', Float64)
-        pcofs = pcofs[1:300:end, :] # Shorten the data for now
-        pcof_rows = copy.(eachrow(pcofs)) # Copy to convert to plain arrays, not views
+    out_filename = "comparison_" * basename(filename)
+    open(out_filename, "w") do io
+        DelimitedFiles.writedlm(io, rpad.(header, 24), ',')
+    end
 
-        data = @distributed (vcat) for (iter_n, pcof) in collect(enumerate(pcof_rows))
-            println("[ $(now()) | worker  $(myid()) ] Doing iteration $iter_n")
+    for (i_chunk, pcof_rows_chunk) in enumerate(pcof_rows_chunked)
+        println("[ $(now()) | worker  $(myid()) ] Starting chunk $i_chunk")
+
+        data = @distributed (vcat) for (iter_n, pcof) in collect(enumerate(pcof_rows_chunk))
+            ipopt_iter = ((i_chunk-1)*chunk_size+iter_n)
+            println("[ $(now()) | worker  $(myid()) ] Doing iteration $ipopt_iter")
 
             # Allocate working arrays
             coarse_history = zeros(coarse_prob.real_system_size, 1+coarse_N_deriv, 1+coarse_nsteps, coarse_prob.N_initial_conditions)
@@ -162,23 +163,18 @@ function main()
             ginf_err = abs(coarse_ginf - fine_ginf)
 
             data_row = hcat(
-                iter_n, UT_err, grad_pcof_err, inf_err, ginf_err, 
+                ipopt_iter, UT_err, grad_pcof_err, inf_err, ginf_err, 
                 coarse_inf, coarse_ginf, coarse_norm_grad, coarse_norm_UT,
                 fine_inf, fine_ginf, fine_norm_grad, fine_norm_UT,
             )
         end
-        
-        out_filename = "comparison_" * basename(filename)
 
-        open(out_filename, "w") do io
-            DelimitedFiles.writedlm(io, rpad.(header, 24), ',')
-        end
         open(out_filename, "a+") do io
             DelimitedFiles.writedlm(io, rpad.(data, 24), ',')
         end
     end
-
-    return data
+    
+    return nothing
 end
 
 main()
